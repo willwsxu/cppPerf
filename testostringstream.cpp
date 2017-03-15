625f5c2a51cf22c1d03cce5160267987e3788fc0
willwsxu <willwsxu@gmail.com>
2017-03-15 17:16:31 -0500

    revise string test to be more accurate

--- testostringstream.cpp
+++ testostringstream.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 char cstr[200];
 string str;
-ostringstream ostr;
 /*
-string nano seconds :       53000000
-strcpy nano seconds :       29000000
-sprintf_s nano seconds :    292000000
-stringstream nano seconds : 3400000000
+string nano seconds:        54000000
+strcpy nano seconds:        58000000
+sprintf_s nano seconds:    298000000
+stringstream nano seconds: 830000000
+strcpy10 nano seconds:       9450000
+memcpy10 nano seconds:        840000
 */
 void testStringConcat()
 {
@@ -34,13 +35,13 @@ void testStringConcat()
 
 	start = chrono::high_resolution_clock::now();
 	for (int i = 0; i < loops; i++) {
-		strcpy_s(cstr, sizeof(str), "\"");
-		strcat_s(cstr, sizeof(str), "name");
-		strcpy_s(cstr, sizeof(str), "\"");
-		strcpy_s(cstr, sizeof(str), "=");
-		strcpy_s(cstr, sizeof(str), "\"");
-		strcpy_s(cstr, sizeof(str), "value");
-		strcpy_s(cstr, sizeof(str), "\"");
+		strcpy_s(cstr, sizeof(cstr), "\"");
+		strcat_s(cstr, sizeof(cstr), "name");
+		strcat_s(cstr, sizeof(cstr), "\"");
+		strcat_s(cstr, sizeof(cstr), "=");
+		strcat_s(cstr, sizeof(cstr), "\"");
+		strcat_s(cstr, sizeof(cstr), "value");
+		strcat_s(cstr, sizeof(cstr), "\"");
 	}
 	end = chrono::high_resolution_clock::now();
 	nanos = chrono::duration_cast<chrono::nanoseconds> (end - start);
@@ -48,20 +49,35 @@ void testStringConcat()
 
 	start = chrono::high_resolution_clock::now();
 	for (int i = 0; i < loops; i++) {
-		sprintf_s(cstr, sizeof(str), "%c%s%c=%c%s%c", '"', "name", '"', '"', "value", '"');
+		sprintf_s(cstr, sizeof(cstr), "%c%s%c=%c%s%c", '"', "name", '"', '"', "value", '"');
 	}
 	end = chrono::high_resolution_clock::now();
 	nanos = chrono::duration_cast<chrono::nanoseconds> (end - start);
 	cout << "sprintf_s nano seconds: " << nanos.count() << endl;
 
 	start = chrono::high_resolution_clock::now();
-	for (int i = 0; i < loops/100; i++) {
+	for (int i = 0; i < loops / 10; i++) {
+		ostringstream ostr;
 		ostr << '"' << "name" << "\"" << "=" << "\"" << "value" << "\"";
 		str = ostr.str();
-		ostr.clear();
 	}
 	end = chrono::high_resolution_clock::now();
 	nanos = chrono::duration_cast<chrono::nanoseconds> (end - start);
-	cout << "stringstream nano seconds: " << nanos.count()*100 << endl;
+	cout << "stringstream nano seconds: " << nanos.count()*10 << endl;
 
+	start = chrono::high_resolution_clock::now();
+	for (int i = 0; i < loops*100; i++) {
+		strcpy_s(cstr, sizeof(cstr), "name=value"); // copy 10 bytes
+	}
+	end = chrono::high_resolution_clock::now();
+	nanos = chrono::duration_cast<chrono::nanoseconds> (end - start);
+
+	start = chrono::high_resolution_clock::now();
+	for (int i = 0; i < loops*100; i++) {
+		memcpy(cstr, "name=value", 10); // copy 10 bytes
+		cstr[10] = 0;
+	}
+	end = chrono::high_resolution_clock::now();
+	nanos = chrono::duration_cast<chrono::nanoseconds> (end - start);
+	cout << "memcpy10 nano seconds: " << nanos.count()/100 << endl;
 }
