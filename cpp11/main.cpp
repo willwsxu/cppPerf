b0451b99a1c09da3c237218500c1814438764065
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-06-01 22:35:45 -0500

    slist compare performance of shared_ptr, unique_ptr, and raw ptr

--- cpp11/main.cpp
+++ cpp11/main.cpp
@@ -216,21 +216,70 @@ struct MemoryTracker
 	MemoryTracker() {
 		count++; 
 	}
+	MemoryTracker(const MemoryTracker&&) {
+		count++;
+	}
 	~MemoryTracker()
 	{
+
 		--count;
 	}
 };
 int MemoryTracker::count=0;
 
-using slistTracker = slist<MemoryTracker>;
+/*
+slist shared_ptr nano seconds: 136080100 count 2
+slist unique_ptr nano seconds:  91674500 count 2
+slist raw ptr nano seconds:     76840000 count 2
+*/
 TEST_CASE("slist single thread memory tracker", "SLIST")
 {
+	using slistTracker = slist<MemoryTracker>;
 	slistTracker simple;
-	simple.push_front(MemoryTracker());
+	simple.push_front(MemoryTracker());  // copy constructor, move constructor
 	simple.push_front(MemoryTracker());
 	simple.push_front(MemoryTracker());
 	CHECK(MemoryTracker::count == 3);
 	simple.pop_front();
-	CHECK(MemoryTracker::count == 3);
+	CHECK(MemoryTracker::count == 2);
+
+
+	long loops = 1000000;
+	auto perfTest = [](const char *name, auto func) {
+		auto start = chrono::high_resolution_clock::now();
+		long count = func();
+		auto end = chrono::high_resolution_clock::now();
+		auto nanos = chrono::duration_cast<chrono::nanoseconds> (end - start);
+		cout << name << " nano seconds: " << nanos.count() << " count " << count << endl;
+
+	};
+
+	perfTest("slist shared_ptr", [loops]() {
+		slistTracker simple;
+		for (long i = 0; i < loops; i++) {
+			simple.push_front(MemoryTracker());
+			simple.pop_front();
+		}
+		return MemoryTracker::count;
+	});
+
+	using slistTrackerU = slist_u<MemoryTracker>;
+	perfTest("slist unique_ptr", [loops]() {
+		slistTrackerU simple;
+		for (long i = 0; i < loops; i++) {
+			simple.push_front(MemoryTracker());
+			simple.pop_front();
+		}
+		return MemoryTracker::count;
+	});
+
+	using slistTrackerR = slist_r<MemoryTracker>;
+	perfTest("slist raw ptr", [loops]() {
+		slistTrackerR simple;
+		for (long i = 0; i < loops; i++) {
+			simple.push_front(MemoryTracker());
+			simple.pop_front();
+		}
+		return MemoryTracker::count;
+	});
 }
--- cpp11/slist.h
+++ cpp11/slist.h
@@ -8,7 +8,7 @@ class slist
 		T	data;
 		shared_ptr<Node>	next;
 		Node(const T&& d) :data(move(d)) {}
-		Node(const T& d) :data(d) {}
+		Node(const T& d) :data(move(d)) {}
 	};
 	shared_ptr<Node>	head;
 
@@ -23,6 +23,78 @@ public:
 	{
 		if (head) {
 			head = head->next;
+		}
+	}
+
+	const T* peek()
+	{
+		if (head)
+			return &head->data;
+		return nullptr;
+	}
+};
+
+
+template <typename T>
+class slist_r
+{
+	struct Node {
+		T	data;
+		Node*	next;
+		Node(const T&& d) :data(move(d)), next(nullptr){}
+		Node(const T& d) :data(move(d)), next(nullptr) {}
+	};
+	Node *	head;
+
+public:
+	slist_r():head(nullptr)
+	{}
+	void push_front(const T& t)  // cannot use &&
+	{
+		auto n =new Node(t);
+		n->next = head;
+		head = n;
+	}
+	void pop_front()
+	{
+		if (head) {
+			auto old = head;
+			head = head->next;
+			delete old;
+		}
+	}
+
+	const T* peek()
+	{
+		if (head)
+			return &head->data;
+		return nullptr;
+	}
+};
+
+
+template <typename T>
+class slist_u
+{
+	struct Node {
+		T	data;
+		unique_ptr<Node>	next;
+		Node(const T&& d) :data(move(d)) {}
+		Node(const T& d) :data(move(d)) {}
+	};
+	unique_ptr<Node>	head;
+
+public:
+	void push_front(const T& t)  // cannot use &&
+	{
+		auto n = make_unique<Node>(t);
+		n->next = move(head);
+		head = move(n);
+	}
+	void pop_front()
+	{
+		if (head) {
+			head = move(head->next);
 		}
 	}
 
