4c058b989d32abca8e3627d56b9d4545a9035b8d
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-06-18 21:17:35 -0500

    game of life test

--- leetcode/leetcodecpp.cpp
+++ leetcode/leetcodecpp.cpp
@@ -2983,6 +2983,10 @@ public:
 			}
 		}
 	}
+	// 73. Set Matrix Zeroes
+	void setZeroes(vector<vector<int>>& matrix) {
+
+	}
 };
 
 class Triangle2
@@ -3019,8 +3023,18 @@ TEST_CASE("subarray sum", "[SUB]")
 	CHECK(t.subarraySum(vector<int>{1, 1, 1}, 2) == 2);
 }
 
-TEST_CASE("generate matrix", "[NEW]")
+TEST_CASE("generate matrix", "[GEN]")
 {
 	Array2D t;
 	CHECK(t.generateMatrix(3) == vector<vector<int>>{ {1, 2, 3}, { 8, 9, 4 }, { 7, 6, 5 }});
+}
+TEST_CASE("Game of life", "[NEW]")
+{
+	Array2D t;
+	SECTION("normal case") {
+		vector<vector<int>> b{ { 0,1,0 },{ 0,0,1 },{ 1,1,1 },{ 0,0,0 } };
+		vector<vector<int>> ans{ { 0,0,0 },{ 1,0,1 },{ 0,1,1 },{ 0,1,0 } };
+		t.gameOfLife(b);
+		CHECK(b == ans);
+	}
 }
