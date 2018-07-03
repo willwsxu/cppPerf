4cd7c67bb370e418548bf39c621ab4e4d43b1460
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-07-02 20:06:34 -0500

    twitter pass but slow, beat 7%

--- leetcode/design.cpp
+++ leetcode/design.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 class Twitter {
-	unordered_map<int, vector<pair<int, int>>> userTweets;  // store tweets per user. Flaws: redundant, unable to keep time sorted
-	unordered_map<int, vector<int>> followers;  // map person to people who follow
-												//	unordered_map<int, vector<int>> followees;  // map person to people to whom he/she follow
+	vector<pair<int, int>> allTweets;
+	unordered_map<int, unordered_set<int>> followers;  // map person to his or her followers
+	unordered_map<int, unordered_set<int>> followees;  // map person to people to whom he/she follow
 
 public:
 	/** Initialize your data structure here. */
@@ -16,9 +16,7 @@ public:
 
 	/** Compose a new tweet. */
 	void postTweet(int userId, int tweetId) {
-		userTweets[userId].emplace_back(userId, tweetId);
-		for (auto follow : followers[userId])
-			userTweets[follow].emplace_back(userId, tweetId);
+		allTweets.emplace_back(userId, tweetId);
 	}
 
 	/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
@@ -26,43 +24,63 @@ public:
 		vector<int> ans;
 		ans.reserve(10);
 		int i = 0;
-		auto &tweets = userTweets[userId];
-		for (auto p = rbegin(tweets); p != rend(tweets); p++) {
-			ans.push_back(p->second);
-			if (++i == 10)
-				break;
+		auto& follow = followees[userId];  // cache map look up, big performance saver
+		for (auto p = rbegin(allTweets); p != rend(allTweets); p++) {
+			if (p->first == userId || follow.count(p->first)) {
+				ans.push_back(p->second);
+				if (++i == 10)
+					break;
+			}
 		}
 		return ans;
 	}
 
 	/** Follower follows a followee. If the operation is invalid, it should be a no-op. */
 	void follow(int followerId, int followeeId) {
-		if (followerId == followeeId)   // check invalid follow (error #2)
-			return;
-		auto &follow = followers[followeeId];
-		if (find(begin(follow), end(follow), followerId) != end(follow))  // already followed
-			return;
-		follow.push_back(followerId);
-
-		// need to add old tweets from followee  (error #1)
-		auto &tweets = userTweets[followeeId];
-		copy_if(begin(tweets), end(tweets), back_inserter(userTweets[followerId]), [followeeId](auto& p) {return p.first == followeeId; });
+		if (followerId != followeeId) { // check invalid follow (error #2)
+			followers[followeeId].insert(followerId);
+			followees[followerId].insert(followeeId);
+		}
 	}
 
 	/** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
 	void unfollow(int followerId, int followeeId) {
-		if (followerId == followeeId)   // check invalid unfollow (error #3)
-			return;
-		auto& follow = followers[followeeId];
-		auto rem = remove_if(begin(follow), end(follow), [followerId](int f) {return followerId == f; });  // remove follower from followee
-		if (rem != follow.end())
-			follow.erase(rem);
+		if (followerId != followeeId) {  // check invalid unfollow (error #3)
+			followers[followeeId].erase(followerId);
 
-		// remove tweets originated from followee
-		auto &tweets = userTweets[followerId];
-		auto rem2 = remove_if(begin(tweets), end(tweets), [followeeId](auto& p) {return followeeId == p.first; });
-		if (rem2 != end(tweets))
-			tweets.erase(rem2, end(tweets));
+			// remove followee from follower set
+			followees[followerId].erase(followeeId);
+		}
 	}
 };
 
+
+TEST_CASE("Tweeter design", "[NEW]")
+{
+	Twitter tw;
+	CHECK(tw.getNewsFeed(1) == vector<int>{});
+	tw.postTweet(1, 5);
+	CHECK(tw.getNewsFeed(1) == vector<int>{5});
+	tw.unfollow(1, 2);
+	tw.follow(1, 2);
+	CHECK(tw.getNewsFeed(2) == vector<int>{});
+	tw.postTweet(2, 6);
+	CHECK(tw.getNewsFeed(1) == vector<int>{6, 5});
+	tw.postTweet(1, 7);
+	tw.postTweet(2, 8);
+	CHECK(tw.getNewsFeed(1) == vector<int>{8, 7, 6, 5});
+
+	tw.follow(3, 1);
+	tw.follow(1, 1);
+	tw.follow(3, 1);
+	CHECK(tw.getNewsFeed(3) == vector<int>{7, 5});
+
+	tw.unfollow(1, 2);
+	CHECK(tw.getNewsFeed(1) == vector<int>{7, 5});
+	tw.postTweet(2, 9);
+	CHECK(tw.getNewsFeed(1) == vector<int>{7, 5});
+	CHECK(tw.getNewsFeed(2) == vector<int>{9, 8, 6});
+
+	tw.follow(1, 2);
+	CHECK(tw.getNewsFeed(1) == vector<int>{9, 8, 7, 6, 5});  // maintain relative order of past tweets
+}
