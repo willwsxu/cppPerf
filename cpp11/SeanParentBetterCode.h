afe2f85d261e72aa43543a87a5251ed1581a7fa7
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-07-08 22:44:30 -0500

    sean parent stuff

---
+++ cpp11/SeanParentBetterCode.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <random>
+
+// 5 Goals
+// no Incomplete types
+// no raw loops
+// No Incidental Data Structures (no raw pointers)
+// no raw synchronization primitives
+// no inheritance
+
+template <typename In, typename Pred>
+In stable_partition(In first, In last, Pred p)
+{
+	auto n = last - first;
+	if (n == 0)
+		return first;
+	if (n == 1)
+		return first + p(*first);
+	auto m = first + n / 2;
+	return rotate(stable_partition(first, m, p), m,
+		stable_partition(m, last, p));
+}
+
+
+template <typename In, typename Pred>
+In stable_partition_position(In first, In last, Pred p)
+{
+	auto n = last - first;
+	if (n == 0)
+		return first;
+	if (n == 1)
+		return first + p(first);
+	auto m = first + n / 2;
+	return rotate(stable_partition_position(first, m, p), m,
+		stable_partition_position(m, last, p));
+}
+
+template <typename In>
+void sort_subrange(In first, In last, In sf, In sl)
+{
+	if (sf == sl)
+		return;
+	if (sf != first) {
+		nth_element(first, sf, last);
+		++sf; // sf is already sorted
+	}
+	partial_sort(sf, sl, last);
+}
+
+template <typename RandI> // I models RandomAccessIterator
+void shuffle(RandI f, RandI l) {
+	//thread_local std::default_random_engine engine;    
+	std::random_device rd;
+	thread_local std::mt19937 engine(rd());
+	shuffle(f, l, engine);
+}
+template<typename Container>
+void shuffle(Container& cont)
+{
+	shuffle(begin(cont), end(cont));
+}
+
+template<typename RandI>
+std::pair<RandI, RandI> slide(RandI first, RandI last, RandI to)
+{
+	if (to < first) return{ to, rotate(to, first, last) }; // slide up
+	if (to > last) return{ rotate(first, last, to), to }; // slide down
+	return{ first, last };
+}
+
+template<typename BiDirI, typename UPred>
+std::pair<BiDirI, BiDirI> gather(BiDirI first, BiDirI last, BiDirI to, UPred p)
+{
+	return{ stable_partition(first, to, not1(p)), stable_partition(to, last, p) };
+}
+
--- cpp11/SeanParentTest.cpp
+++ cpp11/SeanParentTest.cpp
@@ -1,4 +1,4 @@
-#include "seanparentAlgorithm.h"
+#include "seanparentBetterCode.h"
 
 #include <vector>
 #include <iterator>
@@ -15,4 +15,24 @@ TEST_CASE("stable partition position", "[NEW]")
 	vector<int> ans;
 	copy(begin(target), x, back_inserter(ans));
 	CHECK(ans == vector<int>{1, 4, 9});
+}
+
+TEST_CASE("sort subrange", "[SUBR]")
+{
+	vector<int>  target{ 1,2,3,4,5,6,7,8,9 };
+	shuffle(target);
+	sort_subrange(begin(target), end(target), begin(target) + 3, begin(target) + 6);
+	vector<int> ans;
+	copy(begin(target)+3, begin(target) + 6, back_inserter(ans));
+	CHECK(ans == vector<int>{4,5,6});
+}
+
+TEST_CASE("slide", "[NEW]")
+{
+	vector<int>  target{ 1,2,3,4,5,6,7,8,9 };
+	slide(begin(target)+3, begin(target) + 6, begin(target)); // slide up
+	CHECK(target == vector<int>{4, 5, 6,1,2,3,7,8,9});
+
+	slide(begin(target), begin(target) + 3, begin(target)+6); // slide down
+	CHECK(target == vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9});
 }
