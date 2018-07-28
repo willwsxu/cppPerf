d3686ea7ab0caa5309c90d22f7d874c496e6facb
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-07-27 19:57:22 -0500

    8 bit manipulation

--- leetcode/BitManip.cpp
+++ leetcode/BitManip.cpp
@@ -51,3 +51,218 @@ public:
 		return{ accumulate(nums.begin(), second, 0, bit_xor<int>()), accumulate(second, nums.end(), 0, bit_xor<int>()) };
 	}
 };
+
+
+class Bit
+{
+public:
+	// 338. Counting Bits from 0 to n
+	// dp[0]=0, dp[1]=dp[0]+1, dp[2]=dp[2/2]+0, dp[3]=dp[3/2]+1
+	vector<int> countBits(int num) {
+		vector<int> ans;   // speed up to use push_back instead of initializing to 0. beat 99%
+		ans.reserve(num + 1);
+		ans.push_back(0);
+		if (num)
+			ans.push_back(1);
+		else
+			return ans;
+		int loop = num % 2 ? num : num - 1;
+		for (int i = 2; i <= loop; i += 2) {  // no speed difference using loop step 2
+											  //ans.push_back(ans[i >> 1] + (i & 1));  // divide by 2, plus checking last digit
+			ans.push_back(ans[i >> 1]);  // even number
+			ans.push_back(ans[i] + 1);   // next odd
+		}
+		if (loop<num)  // last even
+			ans.push_back(ans[num >> 1]);
+		return ans;
+	}
+
+	// 461. Hamming Distance, the number of positions at which the corresponding bits are different
+	int hammingDistance(int x, int y) {  // beat 100%
+		x ^= y;
+		int cnt = 0;
+		while (x) {
+			cnt++;
+			x &= x - 1;  // remove right most 1
+		}
+		return cnt;
+	}
+
+	// 477. Total Hamming Distance, between all pairs of the given numbers
+	int totalHammingDistance(vector<int>& nums) {  // beat 61%
+		int ans = 0;
+		vector<int> count(2, 0);
+		for (int i = 0; i < 31; i++) { // check each bit set or not set
+			int mask = 1 << i;
+			for (int n : nums) {
+				count[(n&mask) ? 1 : 0]++;
+			}
+			ans += count[0] * count[1];
+			count[0] = count[1] = 0;
+		}
+		return ans;
+	}
+
+	// 201 Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive
+	int rangeBitwiseAnd(int m, int n) { // beat 98%
+		if (m == n || m == 0)  // speacial cases
+			return m;
+		int d1 = log2(m);    // find highest bit set
+		int d2 = log2(n);
+		if (d1 != d2)        // 1111 0111 in between there is 1000 so all bit AND is 0
+			return 0;
+		int ans = 1 << d1;
+		for (int i = d1 - 1; i >= 0; i--) {  // beat 40% without code above to deal with special case
+			int mask = 1 << i;
+			if ((mask&m) && (mask&n))  // each bit from left to right, if both 1, add to answer
+				ans |= mask;
+			else if ((mask&m) != (mask&n))  // when we reach a position one bit is 1, another is 0, AND must be 0 for rest of the bits
+				break;
+		}
+		return ans;
+	}
+
+	// 318. Maximum Product of Word Lengths
+	int maxProduct(vector<string>& words) {  // beat 36% O(n^2)
+		vector<int> masks;
+		vector<int> lens;
+		int n = words.size();
+		masks.reserve(n);
+		lens.reserve(n);
+		transform(words.begin(), words.end(), back_inserter(masks), [](const string &w) {
+			return accumulate(w.begin(), w.end(), 0, [](int acc, char c) { return acc | (1 << (c - 'a')); });
+		});
+		transform(words.begin(), words.end(), back_inserter(lens), [](const string &w) {
+			return w.size();
+		});
+		int ans = 0;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n; j++) {
+				if ((masks[i] & masks[j]) == 0)  // no overlap, watch out for operator precedence
+					ans = max(ans, lens[i] * lens[j]);
+			}
+		}
+		return ans;
+	}
+
+	// 393. UTF-8 Validation, each int in the vector is a byte
+	bool validUtf8(vector<int>& data) {  // beat 12%
+		const int singl = 0x80;
+		const int doubl = 0xC0;
+		const int tripl = 0xE0;
+		const int quart = 0xF0;
+		const int penta = 0xF8;
+		int bytes = 0;
+		for (int n : data) {
+			if (bytes > 0) {
+				if ((n&doubl) != singl)
+					return false;
+				bytes--;
+				continue;
+			}
+			if ((n&singl) == 0)   // 1 byte, start with 0
+				continue;
+			int t = n&tripl; // 1110 3 bytes
+			switch (t) {
+			case doubl:	bytes = 1;		break;
+			case tripl:
+				if ((n&quart) == tripl)
+					bytes = 2;
+				else if ((n&penta) == quart)
+					bytes = 3;
+				else
+					return false;
+				break;
+			default:
+				return false;
+			}
+		}
+		return bytes == 0;
+	}
+
+	// 397. Integer Replacement. What is the minimum number of replacements needed for n to become 1?
+	// If n is even, replace n with n/2.
+	// If n is odd, you can replace n with either n + 1 or n - 1.
+	int integerReplacement(int n) {
+		int ones = 0;  // count 1 from right
+		int ans = 0;
+		while (n > 1) {
+			if (n & 1)  // count consecutive ones
+				ones++;
+			else if (ones>0) {  // some consecutive ones up to this position
+				if (ones == 1)  // do n-1 operation
+					ones = 0;
+				else
+					ones = 1;   // do n+1 op, thus convert current 0 to 1
+				ans++;  // extra operation regardless n+1 or n-1
+			}
+			ans++;
+			n >>= 1;
+		}
+		return ones ? (ones>1 ? ans + 2 : ans + 1) : ans; // if ones>1, add 2 because n+1 causing n extra bit
+	}
+	// 187. Repeated DNA Sequences, A C G T
+	// Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule
+	// idea: map ACGT to 0123, 2 bits
+	// scan string from left to right, convert each letter to 2 bit and add to an accumulator (shift left 2 bits before adding new bits)
+	// store the 20 bits int in a map
+	vector<string> findRepeatedDnaSequences(string s) { // beat 93%
+		int n = s.size();
+		if (n < 10)
+			return{};
+		int lookup[20] = { 0 };  // define ACGT
+		lookup['C' - 'A'] = 1;
+		lookup['G' - 'A'] = 2;
+		lookup['T' - 'A'] = 3;
+		unordered_map<int, int> subDna;  // 10 letter substring key, map to count
+		int key = 0;
+		for (int i = 0; i < 10; i++) {
+			key <<= 2;
+			key |= lookup[s[i] - 'A'];
+		}
+		vector<string> ans;
+		subDna[key] = 1;
+		for (int i = 10; i < n; i++) {
+			key <<= 2;
+			key |= lookup[s[i] - 'A'];
+			key &= 0xFFFFF;  // keep only 20 bits
+			auto& count = subDna[key];
+			if (count == 1)
+				ans.push_back(s.substr(i - 9, 10));
+			count++;
+		}
+		return ans;
+	}
+};
+
+
+TEST_CASE("total hamming distance", "[NEW]")
+{
+	CHECK(Bit().totalHammingDistance(vector<int>{4, 14, 2}) == 6);
+}
+TEST_CASE("range bit and", "[NEW]")
+{
+	CHECK(Bit().rangeBitwiseAnd(1, 2) == 0);
+	CHECK(Bit().rangeBitwiseAnd(0, 0) == 0);
+	CHECK(Bit().rangeBitwiseAnd(0, 1) == 0);
+	CHECK(Bit().rangeBitwiseAnd(3, 3) == 3);
+	CHECK(Bit().rangeBitwiseAnd(5, 7) == 4);
+	CHECK(Bit().rangeBitwiseAnd(6, 7) == 6);
+	CHECK(Bit().rangeBitwiseAnd(14, 15) == 14);
+}
+
+TEST_CASE("valid UTF", "[NEW]")
+{
+	CHECK(Bit().validUtf8(vector<int>{197, 130, 1}) == true);
+	CHECK(Bit().validUtf8(vector<int>{235, 140, 4}) == false);
+}
+
+TEST_CASE("integer replacements", "[NEW]")
+{
+	CHECK(Bit().integerReplacement(1) == 0);
+	CHECK(Bit().integerReplacement(2) == 1);
+	CHECK(Bit().integerReplacement(3) == 2);
+	CHECK(Bit().integerReplacement(7) == 4);
+	CHECK(Bit().integerReplacement(31) == 6);
+	CHECK(Bit().integerReplacement(8) == 3);
+}
