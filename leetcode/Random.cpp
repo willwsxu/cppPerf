6dea521745e10a605871db1bd235cedae2d9c239
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-08-11 20:56:14 -0500

    pick random node from linked list, beat 73%

--- leetcode/Random.cpp
+++ leetcode/Random.cpp
@@ -63,3 +63,28 @@ TEST_CASE("398. Random Pick Index", "[NEW]")
 	CHECK(rs.pick(1) == 0);
 	CHECK(rs.pick(1) == 0);
 }
+
+#include "ListNode.h"
+// 382. Linked List Random Node
+class Solution {  // beat 73%
+	ListNode *head_;
+	std::random_device rd;  // same idea as ReservoirSampling
+	std::mt19937 g;
+public:
+	/** @param head The linked list's head.
+	Note that the head is guaranteed to be not null, so it contains at least one node. */
+	Solution(ListNode* head):head_(head), g(rd()){	}
+
+	/** Returns a random node's value. */
+	int getRandom() {
+		int ret = 0;
+		int count = 0;
+		ListNode *cur = head_;
+		while (cur) {
+			if (uniform_int_distribution<>(0, count++)(g) == 0)  // 1 of count chance to pick new number
+				ret = cur->val;
+			cur = cur->next;
+		}
+		return ret;
+	}
+};
