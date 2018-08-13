d5be90a1c01482beb39344da57e8781734f82e5f
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-08-12 22:35:53 -0500

    refactoring string builder

--- bmtests/teststringbuilder.cpp
+++ bmtests/teststringbuilder.cpp
@@ -12,20 +12,13 @@ using namespace std;
 
 BENCHMARK_MAIN();
 
-static const int MAX_SIZE = (8 << 10)+100;
+static const int MAX_SIZE = 5*((8 << 10)+100);
 char cstr[MAX_SIZE];
 string str;
 int res=0;
 /*
-string nano seconds:        53000000
-strcpy nano seconds:        55000000
-sprintf_s nano seconds:    284000000
-stringstream nano seconds: 740000000
-strcpy_s10 nano seconds:     9760000
-memcpy10 nano seconds:        820000
 strncmp10 nano seconds:      7570000
 memcmp10 nano seconds:             0
-string==10 nano seconds:     2270000
 */
 
 static void BM_string_plus(benchmark::State& state) {
@@ -34,15 +27,10 @@ static void BM_string_plus(benchmark::State& state) {
 	char *src = s.get();
 	str.reserve(MAX_SIZE);
 	for (auto _ : state) {
-		str = '"';
-		str += "name";
-		str += '"';
-		str += '=';
-		str += '"';
-		str += src;
-		str += '"';
+		str = src;  // 
+		str += src;		str += src;		str += src;		str += src;
 	}
-	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)*5));
 }
 BENCHMARK(BM_string_plus)->Range(8, 8 << 10);
 
@@ -52,10 +40,10 @@ static void BM_string_append(benchmark::State& state) {
 	char *src = s.get();
 	str.reserve(MAX_SIZE);
 	for (auto _ : state) {
-		str = '"';
-		str.append("name").append(1, '"').append(1, '=').append(1, '"').append(src).append(1, '"');
+		str = src;
+		str.append(src).append(src).append(src).append(src);
 	}
-	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)*5));
 }
 BENCHMARK(BM_string_append)->Range(8, 8 << 10);
 
@@ -66,9 +54,9 @@ static void BM_string_builder(benchmark::State& state) {
 	char *src = s.get();
 	for (auto _ : state) {
 		sb.clear();
-		sb.append(1, '"').append("name").append(1, '"').append(1, '=').append(1, '"').append(src).append(1, '"');
+		sb.append(src).append(src).append(src).append(src).append(src);
 	}
-	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)*5));
 }
 BENCHMARK(BM_string_builder)->Range(8, 8 << 10);
 
@@ -77,15 +65,13 @@ static void BM_strcat_s(benchmark::State& state) {
 	auto s = memset_char('x', sz);
 	char *src = s.get();
 	for (auto _ : state) {
-		strcpy_s(cstr, sizeof(cstr), "\"");
-		strcat_s(cstr, sizeof(cstr), "name");
-		strcat_s(cstr, sizeof(cstr), "\"");
-		strcat_s(cstr, sizeof(cstr), "=");
-		strcat_s(cstr, sizeof(cstr), "\"");
+		strcpy_s(cstr, sizeof(cstr), src);
 		strcat_s(cstr, sizeof(cstr), src);
-		strcat_s(cstr, sizeof(cstr), "\"");
+		strcat_s(cstr, sizeof(cstr), src);
+		strcat_s(cstr, sizeof(cstr), src);
+		strcat_s(cstr, sizeof(cstr), src);
 	}
-	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	state.SetBytesProcessed(int64_t(state.iterations()) * strlen(cstr));
 }
 BENCHMARK(BM_strcat_s)->Range(8, 8 << 10);
 
@@ -95,9 +81,9 @@ static void BM_sprintf_s(benchmark::State& state) {
 	auto s = memset_char('x', sz);
 	char *src = s.get();
 	for (auto _ : state) {
-		sprintf_s(cstr, sizeof(cstr), "%c%s%c=%c%s%c", '"', "name", '"', '"', src, '"');
+		sprintf_s(cstr, sizeof(cstr), "%s%s%s%s%s", src, src, src, src, src);
 	}
-	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	state.SetBytesProcessed(int64_t(state.iterations()) * strlen(cstr));
 }
 BENCHMARK(BM_sprintf_s)->Range(8, 8 << 10);
 
@@ -107,10 +93,10 @@ static void BM_ostringstream(benchmark::State& state) {
 	char *src = s.get();
 	for (auto _ : state) {
 		ostringstream ostr;
-		ostr << '"' << "name" << "\"" << "=" << "\"" << src << "\"";
+		ostr << src << src << src << src << src;
 		str = ostr.str();
 	}
-	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(str.size()));
 }
 BENCHMARK(BM_ostringstream)->Range(8, 8 << 10);
 
