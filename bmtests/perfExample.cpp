4efc605482d88158ece0a4dd2d7ef642ce562633
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-08-15 19:59:16 -0500

    add genral performance benchmark proj

--- bmtests/perfExample.cpp
+++ bmtests/perfExample.cpp
@@ -2,66 +2,13 @@
 #include <string>
 #include <memory>
 #include <functional>
-//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-//#include "catch.hpp"
 #include <benchmark/benchmark.h>
 
 BENCHMARK_MAIN();
 
 using namespace std;
 
-
-static void BM_post_increment(benchmark::State& state) {
-	int a[10];
-	for (auto _ : state)
-	{
-		int i = 0;
-		while (i < 10)
-			a[i++] = 0;
-	}
-	//std::cout << a[0] << std::endl;
-}
-
-static void BM_pre_increment(benchmark::State& state) {
-	int a[10];
-	for (auto _ : state)
-	{
-		int i = -1;
-		while (i < 9)
-			a[++i] = 0;
-	}
-	//std::cout << a[0] << std::endl;
-}
-
-static void BM_pre_increment2(benchmark::State& state) {
-	int a[10];
-	int b[10];
-	for (auto _ : state)
-	{
-		int i = -1;
-		int j = -1;
-		while (i < 9) {
-			a[++i] = 0;
-			b[++j] = 0;
-		}
-	}
-	//std::cout << a[0] << " " << b[0] << std::endl;
-}
-
-static void BM_post_increment2(benchmark::State& state) {
-	int a[10];
-	int b[10];
-	for (auto _ : state)
-	{
-		int i = 0;
-		int j = 0;
-		while (i < 10) {
-			a[i++] = 0;
-			b[j++] = 0;
-		}
-	}
-	//std::cout << a[0] << " " << b[0] << std::endl;
-}
+#include "bm_performance_general\pre_post_increment.h"
 
 static void BM_int_div(benchmark::State& state) {
 	for (auto _ : state)
@@ -87,12 +34,7 @@ static void BM_int_no_div(benchmark::State& state) {
 		}
 	}
 }
-/*
-BENCHMARK(BM_post_increment);  //1
-BENCHMARK(BM_pre_increment);   //6
-BENCHMARK(BM_post_increment2); //1
-BENCHMARK(BM_pre_increment2);  //7
-*/
+
 /*BENCHMARK(BM_int_div);
 BENCHMARK(BM_int_no_div);*/
 
