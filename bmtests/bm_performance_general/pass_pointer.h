3d0c355daeefa7c9b4c2d314a09302e167e1ee42
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-08-16 23:10:23 -0500

    pass pointer benchmark

--- bmtests/bm_performance_general/pass_pointer.h
+++ bmtests/bm_performance_general/pass_pointer.h
@@ -20,10 +20,10 @@ static void BM_pass_shared_ptr_copy(benchmark::State& state) {
 }
 static void BM_pass_unique_ptr(benchmark::State& state) {
 	auto d = make_unique<Derived>("test");
-	auto good = [](const unique_ptr<Base> ) {};
+	auto good = [](unique_ptr<Derived> x) { return move(x); };
 	for (auto _ : state)
 	{
-		good(move(d));
+		d=good(move(d));
 	}
 }
 static void BM_pass_raw_reference(benchmark::State& state) {
@@ -44,7 +44,7 @@ static void BM_pass_raw_pointer(benchmark::State& state) {
 }
 
 BENCHMARK(BM_pass_shared_ptr_copy); // 15ns
-BENCHMARK(BM_pass_shared_ptr_ref);  // 15ns
-BENCHMARK(BM_pass_unique_ptr);		// 1ns
+BENCHMARK(BM_pass_shared_ptr_ref);  // 14ns
+BENCHMARK(BM_pass_unique_ptr);		// 2ns
 BENCHMARK(BM_pass_raw_reference);   // 1ns
 BENCHMARK(BM_pass_raw_pointer);   // 1ns
