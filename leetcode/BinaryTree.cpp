3fcb3484d2b8fe24ae5557da0bae34a42bd2b634
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-09-11 22:33:46 -0500

    trim BST to value between L and R

--- leetcode/BinaryTree.cpp
+++ leetcode/BinaryTree.cpp
@@ -1219,5 +1219,18 @@ public:
 
 class BST
 {
+public:
+	// 669. Trim a Binary Search Tree
+	TreeNode* trimBST(TreeNode* root, int L, int R) { // beat 27%
+		if (root == nullptr)
+			return root;
+		if (root->val > R)
+			return trimBST(root->left, L, R);
+		if (root->val < L)
+			return trimBST(root->right, L, R);
+		root->left = trimBST(root->left, L, R);
+		root->right = trimBST(root->right, L, R);
+		return root;
+	}
 
 };
