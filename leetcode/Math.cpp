276e9a37fbb00b4b946804abe014600742b0f7c0
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-09-22 21:57:45 -0500

    triangle area, beat 94%

--- leetcode/Math.cpp
+++ leetcode/Math.cpp
@@ -389,6 +389,25 @@ public:
 			return num % 10;
 		}
 	}
+
+	// 812. Largest Triangle Area
+	double largestTriangleArea(vector<vector<int>>& points) { // beat 94%
+		int n = points.size();
+		if (n < 3)
+			return 0;
+		double area = 0;
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {//formular Area=(Ax/(By-Cy)+Bx(Cy-Ay)+Cx(Ay-By))/2
+					double a = points[i][0] * (points[j][1] - points[k][1]);
+					a += points[j][0] * (points[k][1] - points[i][1]);
+					a += points[k][0] * (points[i][1] - points[j][1]);
+					area = max(area, abs(a / 2.0));
+				}
+			}
+		}
+		return area;
+	}
 };
 TEST_CASE("400. Find the nth digit", "[NEW]")
 {
