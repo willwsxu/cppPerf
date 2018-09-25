2b32927b7dedf44524d81d299b1740acf326f9fd
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-09-24 22:14:07 -0500

    string generator, beat 68%

--- leetcode/STL-no_rawloop.cpp
+++ leetcode/STL-no_rawloop.cpp
@@ -20,4 +20,19 @@ public:
 		}
 		return A;
 	}
+	// 412. Fizz Buzz
+	vector<string> fizzBuzz(int n) {  // generate beat 68%
+		vector<string> ans(n, "");
+		std::generate(ans.begin(), ans.end(), [n = 0]() mutable->string { 
+			if (++n % 15 == 0)
+				return "FizzBuzz";
+			else if (n % 5 == 0)
+				return "Buzz";
+			else if (n % 3 == 0)
+				return "Fizz";
+			else
+				return to_string(n);
+		});
+		return ans;
+	}
 };
