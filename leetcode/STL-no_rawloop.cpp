853024d5f68b1aa3f4099b0ab46b29bf96d22064
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-10-06 14:15:49 -0500

    6 proj using STL

--- leetcode/STL-no_rawloop.cpp
+++ leetcode/STL-no_rawloop.cpp
@@ -138,7 +138,113 @@ public:
 		}
 		return{ 0,0 };
 	}
+
+	// 896. Monotonic Array, An array A is monotone increasing if for all i <= j, A[i] <= A[j]
+	// An array A is monotone decreasing if for all i <= j, A[i] >= A[j]
+	bool isMonotonic(vector<int>& A) {  // beat 98% on 5th submit
+		if (A.size() < 3)
+			return true;
+		auto notEq = adjacent_find(A.begin(), A.end(), not_equal_to<>()); // find first not equal
+		if (notEq == A.end())  // all same
+			return true;
+		auto start = notEq + 1;  // staring point for next find
+		if (*notEq < *start)
+			return adjacent_find(start, A.end(), greater<>()) == A.end();
+		return adjacent_find(start, A.end(), less<>()) == A.end();
+	}
+	// 506. Relative Ranks
+	vector<string> findRelativeRanks(vector<int>& nums) {  // beat 99%
+		vector<pair<int, int>> score_pos; // keep track of position of score
+		score_pos.reserve(nums.size());
+		for (size_t i = 0; i < nums.size(); i++) {
+			score_pos.emplace_back(i, nums[i]);
+		}
+		sort(score_pos.begin(), score_pos.end(), [](const auto&p1, const auto&p2) { return p1.second > p2.second; }); // sort scorein reverse
+		vector<string> ans(nums.size());
+		const char*top3[] = { "Gold Medal", "Silver Medal", "Bronze Medal" };
+		for (int i = 0; i < min(3, (int)score_pos.size()); i++)
+			ans[score_pos[i].first] = top3[i];
+		for (size_t i = 3; i < score_pos.size(); i++)
+			ans[score_pos[i].first] = to_string(i + 1);
+		return ans;
+	}
+	// 724. Find Pivot Index, sum of left =sum of right, 
+	int pivotIndex(vector<int>& nums) {  // beat 97%
+		if (nums.empty())  // special case
+			return -1;
+		partial_sum(nums.begin(), nums.end(), nums.begin());
+		int last = nums.size() - 1;
+		if (nums[last] == nums[0])  // special case
+			return 0;
+		for (int i = 1; i < last; i++)  // iterate from second to second to last
+			if (nums[i - 1] == nums[last] - nums[i])
+				return i;
+		if (nums[last - 1] == 0)
+			return last;
+		return -1;
+	}
+	// 643. Maximum Average Subarray I, over k elements
+	double findMaxAverage(vector<int>& nums, int k) {  // beat 66%
+		if (k == 1)
+			return *max_element(nums.begin(), nums.end());
+		int k_sum = accumulate(nums.begin(), nums.begin() + k - 1, 0); // sum of k-1 
+		double ans = INT32_MIN;
+		for (size_t i = k - 1; i < nums.size(); i++) {
+			k_sum += nums[i];
+			ans = max(ans, (double)k_sum / k);
+			k_sum -= nums[i + 1 - k];  // shift sum to right
+		}
+		return ans;
+	}
+	// 747. Largest Number At Least Twice of Others
+	int dominantIndex(vector<int>& nums) {  // beat 98%
+		auto max_elem = max_element(nums.begin(), nums.end());
+		*max_elem /= 2;
+		if (all_of(nums.begin(), nums.end(), [max_num = *max_elem](int n) {return n <= max_num; }))
+			return distance(nums.begin(), max_elem);
+		return -1;
+	}
+	// 581. Shortest Unsorted Continuous Subarray
+	int findUnsortedSubarray(vector<int>& nums) {  // beat 76%
+		auto sorted = adjacent_find(nums.begin(), nums.end(), [](int n1, int n2) {return n2 < n1; });
+		if (sorted == nums.end())
+			return 0;
+		// sorted is the end of sorted at front, exclusive
+		auto min_elem = min_element(++sorted, nums.end());// if there is any number less than values in the sorted, adjust
+		sorted = lower_bound(nums.begin(), sorted, *min_elem);
+		if (*sorted == *min_elem) {  // adjust to one pass last valid item
+			++sorted;
+		}
+		auto sorted_back = adjacent_find(nums.rbegin(), nums.rend(), [](int n1, int n2) {return n2 > n1; }).base(); // sorted_back is the first of sorted at back
+		auto max_elem = max_element(sorted, sorted_back); // if there is any number less than values in the sorted_back, adjust
+		sorted_back = lower_bound(sorted_back, nums.end(), *max_elem);
+		return distance(sorted, sorted_back);
+	}
 };
+TEST_CASE("581. Shortest Unsorted Continuous Subarray", "[NEW]")
+{
+	CHECK(STL().findUnsortedSubarray(vector<int>{1, 3, 2, 3, 3}) == 2);
+	CHECK(STL().findUnsortedSubarray(vector<int>{2, 6, 2, 1, 16, 9, 15}) == 7);
+	CHECK(STL().findUnsortedSubarray(vector<int>{2, 6, 2, 1, 10, 9, 15}) == 6);
+	CHECK(STL().findUnsortedSubarray(vector<int>{2, 6, 4, 8, 10, 9, 15}) == 5);
+}
+TEST_CASE("724. Find Pivot Index", "[NEW]")
+{
+	CHECK(STL().pivotIndex(vector<int>{}) == -1);
+	CHECK(STL().pivotIndex(vector<int>{-1, -1, 0, 1, 1, -1}) == 0);
+	CHECK(STL().pivotIndex(vector<int>{-1, -1, 0, 1, 1, 2}) == 5);
+	CHECK(STL().pivotIndex(vector<int>{-1, -1, 0, 1, 1, 1}) == 5);
+}
+TEST_CASE("506. Relative Ranks", "[NEW]")
+{
+	CHECK(STL().findRelativeRanks(vector<int>{2, 3}) == vector<string>{"Silver Medal", "Gold Medal"});
+	CHECK(STL().findRelativeRanks(vector<int>{5, 4, 3, 2, 1}) == vector<string>{"Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"});
+}
+TEST_CASE("896. Monotonic Array", "[NEW]")
+{
+	CHECK(STL().isMonotonic(vector<int>{1, 2, 2, 4}) == true);
+
+}
 
 TEST_CASE("697. Degree of an Array", "[NEW]")
 {
