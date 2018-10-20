e49dc9f69134951c34976f163702d36b0b8fb00f
WillX <willwsxu@gmail.com>
2018-10-20 18:54:35 -0500

    implement fast large int multiply

--- cpp11/testLargetInt.cpp
+++ cpp11/testLargetInt.cpp
@@ -6,16 +6,16 @@ TEST_CASE("larget int math", "NEW")
 {
 	LargeInt a(1234), b(9345);
 	LargeInt c = a + b;
-	CHECK(c.get() == vector<LargeInt::DigitType>{1,0,5,7,9});
+	CHECK(c.get() == "10579");
 
 	LargeInt d = b * 2;
-	CHECK(d.get() == vector<LargeInt::DigitType>{1,8,6,9,0});
+	CHECK(d.get() == "18690");
 
 	LargeInt e = a * b;
-	CHECK(e.get() == vector<LargeInt::DigitType>{1,1,5,3,1,7,3,0});
+	CHECK(e.get() == "11531730");
 	LargeInt x(0);
 	x = move(e);
-	CHECK(x.get() == vector<LargeInt::DigitType>{1, 1, 5, 3, 1, 7, 3, 0});
+	CHECK(x.get() == "11531730");
 
 	LargeInt big(1000000000);
 	auto p2 = big*big;
@@ -23,30 +23,37 @@ TEST_CASE("larget int math", "NEW")
 	auto p8 = p4*p4;
 	auto p16 = p8*p8;
 	auto p32 = p16*p16;
-	vector<LargeInt::DigitType> p144(145, 0);
-	p144[0] = 1;
+	string p144(145, '0');
+	p144[0] = '1';
 	CHECK(p16.get() == p144);
-	vector<LargeInt::DigitType> p288(289, 0);
-	p288[0] = 1;
+	string p288(289, '0');
+	p288[0] = '1';
 	CHECK(p32.get() == p288);
 }
 
-vector<LargeInt::DigitType> fibonacciModified(int t1, int t2, int n) {
-	LargeInt lt1(t1);
-	LargeInt lt2(t2);
+string fibonacciModified(int t1, int t2, int n) {
+	const static int SIZE = 110000;
+	LargeInt lt1(t1, SIZE);
+	LargeInt lt2(t2, SIZE);
+	LargeInt output(0, SIZE);
+	LargeInt temp_copy(0, SIZE);
 	while (n-- > 2) {
-		LargeInt lt = lt2*lt2;
-		lt += lt1;
-		lt1=move(lt2);
-		lt2 = move(lt);
+		multiply(output, temp_copy, lt2, lt2);
+		output += lt1;
+		lt1.swap(lt2);
+		lt2.swap(output);
 	}
 	return lt2.get();
 }
 
 TEST_CASE("larget int fibonacci", "NEW")
 {
-	CHECK(fibonacciModified(0, 1, 6) == vector<LargeInt::DigitType>{2,7});
+	CHECK(fibonacciModified(0, 1, 6) == "27");
 
-	CHECK(fibonacciModified(0, 1, 10) == vector<LargeInt::DigitType>{8,4,2,6,6,6,1,3,0,9,6,2,8,1,2,4,3,3,8,2,1,1,2});
-	fibonacciModified(0, 1, 20);
+	CHECK(fibonacciModified(0, 1, 10) == "84266613096281243382112");
+
+	//auto t1=fibonacciModified(1, 1, 20);
+	//CHECK(t1.size()==46952);
+	//auto t2 = fibonacciModified(2, 2, 20);
+	//cout << t2.size() << "\n";
 }
--- library/large_int.h
+++ library/large_int.h
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <string>
+#include <cassert>
 
 using std::vector;
 using std::transform;
@@ -20,50 +22,100 @@ public:
 		lhs += rhs;
 		return lhs;
 	}
-	LargeInt& operator+=(LargeInt& rhs)  // rhs could be modified inside the method, assyme lhs is longer than rhs
+	LargeInt& operator+=(const LargeInt& rhs)
 	{
-		auto *pLong = &li;
-		auto *pShort = &rhs.li;
+		return add(begin(rhs.li), end(rhs.li));
+	}
+	template<typename RandIter>
+	LargeInt& add(RandIter rhs_s, RandIter rhs_e)
+	{
 		int carry = 0;
-		transform(pShort->begin(), pShort->end(), pLong->begin(), pLong->begin(), [&carry](DigitType c1, DigitType c2) {
+		transform(rhs_s, rhs_e, begin(li), begin(li), [&carry](DigitType c1, DigitType c2) {
 			int sum = c1 + c2 + carry;
 			carry = sum / 10;
 			return static_cast<DigitType>(sum % 10);
 		});
-		if (carry && pLong->size() > pShort->size()) {
-			auto start = pLong->begin() + pShort->size();
-			transform(start, pLong->end(), start, [&carry](DigitType c1) {
+		size_t short_size = distance(rhs_s, rhs_e);
+		if (carry && li.size() > short_size) {
+			auto start = begin(li) + short_size;
+			transform(start, end(li), start, [&carry](DigitType c1) {
 				int sum = c1 + carry;
 				carry = sum / 10;
 				return static_cast<DigitType>(sum % 10);
 			});
 		}
 		if (carry)
-			pLong->push_back(static_cast<DigitType>(carry));
+			li.push_back(static_cast<DigitType>(carry));
 		return *this;
 	}
-	friend LargeInt operator*(const LargeInt& lhs, const LargeInt& rhs)
+
+	template<typename RandIter>
+	friend LargeInt add(RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e);
+
+	LargeInt& operator-=(const LargeInt& rhs)
 	{
-		LargeInt ans(0);
-		LargeInt temp_copy(0);
-		const int out_size = lhs.li.size() + rhs.li.size() + 5;
-		temp_copy.li.reserve(out_size);
-		ans.li.reserve(out_size);
-		int shift = 0;
-		for (DigitType d : rhs.li) {
-			temp_copy.li.assign(shift++, 0); // add some zero before copy from lhs
-			std::copy(begin(lhs.li), end(lhs.li), back_inserter(temp_copy.li));
-			temp_copy *= d;
-			if (ans.li.size() < temp_copy.li.size())
-				std::swap(ans.li, temp_copy.li);
-			ans += temp_copy;
+		int borrow = 0;
+		transform(begin(rhs.li), end(rhs.li), begin(li), begin(li), [&borrow](DigitType c1, DigitType c2) {
+			int sum = c1 - c2 - borrow;
+			if (sum < 0) {
+				borrow = 1;
+				sum += 10;
+			}
+			else
+				borrow = 0;
+			return static_cast<DigitType>(sum);
+		});
+		size_t short_size = rhs.li.size();
+		if (borrow && li.size() > short_size) {
+			auto start = begin(li) + short_size;
+			transform(start, end(li), start, [&borrow](DigitType c1) {
+				int sum = c1 - borrow;
+				if (sum < 0) {
+					borrow = 1;
+					sum += 10;
+				}
+				else
+					borrow = 0;
+				return static_cast<DigitType>(sum);
+			});
 		}
-		//std::cout << "lhs size" << lhs.li.size() << " ans size" << ans.li.size() << "\n";
-		return ans;
+		assert(borrow == 0);
+		return *this;
 	}
 
-	LargeInt(int n)
+	friend void multiply(LargeInt& ans, LargeInt& temp_copy, const LargeInt& lhs, const LargeInt& rhs)
 	{
+		multiply(ans, temp_copy, begin(lhs.li), end(lhs.li), begin(rhs.li), end(rhs.li));
+	}
+
+	template<typename RandIter>
+	friend void multiply(LargeInt& ans, LargeInt& temp_copy, RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e)
+	{
+		ans.li.assign(1, 0);
+		int power = 0;
+		for (auto y = rhs_s; y != rhs_e; ++y, power++) {
+			if (*y > 0) {
+				temp_copy.li.assign(power, 0); // add some zero before copy from lhs
+				std::copy(lhs_s, lhs_e, back_inserter(temp_copy.li));
+				temp_copy *= (*y);
+				if (ans.li.size() < temp_copy.li.size())
+					std::swap(ans.li, temp_copy.li);
+				ans += temp_copy;
+			}
+		}
+	}
+	template<typename RandIter>
+	friend LargeInt multiply1(RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e);
+	friend LargeInt operator*(const LargeInt& lhs, const LargeInt& rhs);
+
+	// divide operands into half, left half (least siginicant) ceil(n/2), right half floor(n/2)
+	template<typename RandIter>
+	friend LargeInt multiply_fast(RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e);
+	friend LargeInt multiply_fast(LargeInt& lhs, LargeInt& rhs);
+
+	LargeInt(int n, int capacity=5)
+	{
+		li.reserve(capacity);
 		if (n == 0) {
 			li.push_back(static_cast<DigitType>(0));
 			return;
@@ -73,10 +125,11 @@ public:
 			n /= 10;
 		}
 	}
-	vector<DigitType> get() const {
-		auto x = li;
-		std::reverse(begin(x), end(x));
-		return x;
+	std::string get() const {
+		std::string str;
+		str.reserve(li.size() + 1);
+		std::transform(rbegin(li), rend(li), back_inserter(str), [](char c) { return c + '0'; });
+		return str;
 	}
 	LargeInt(LargeInt&& rhs):li(move(rhs.li))
 	{
@@ -107,3 +160,77 @@ private:
 	}
 	std::vector<DigitType> li;  // least significant to most significant digit
 };
+
+template<typename RandIter>
+LargeInt multiply1(RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e)
+{
+	LargeInt ans(0);
+	LargeInt temp_copy(0);
+	const int out_size = distance(lhs_s, lhs_e) + distance(rhs_s, rhs_e) + 5;
+	temp_copy.li.reserve(out_size);
+	ans.li.reserve(out_size);
+	multiply(ans, temp_copy, lhs_s, lhs_e, rhs_s, rhs_e);
+	return ans;
+}
+LargeInt operator*(const LargeInt& lhs, const LargeInt& rhs)
+{
+	//std::cout << "lhs size" << lhs.li.size() << " ans size" << ans.li.size() << "\n";
+	return multiply1(begin(lhs.li), end(lhs.li), begin(rhs.li), end(rhs.li));
+}
+
+// XY = 10^2ceil(n/2) XrYr + 10^ceil(n/2) * [(Xl + Xr)(Yl + Yr) - XlYl - XrYr] + XlYl
+template<typename RandIter>
+LargeInt multiply_fast(RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e)
+{
+	int size_l = distance(lhs_s, lhs_e);
+	int size_r = distance(rhs_s, rhs_e);
+	assert(size_l == size_r);
+	if (size_l < 10) {
+		return multiply1(lhs_s, lhs_e, rhs_s, rhs_e);
+	}
+	int right_half = size_r / 2;
+	int left_half = size_r - right_half;
+	auto XrYr = multiply_fast(lhs_s + left_half, lhs_e, rhs_s + left_half, rhs_e);
+	auto XlYl = multiply_fast(lhs_s, lhs_s + left_half, rhs_s, rhs_s + left_half);
+	LargeInt ans(0, (size_l + 1) << 1);
+	ans.li.assign(left_half << 1, 0);
+	copy(begin(XrYr.li), end(XrYr.li), back_inserter(ans.li));
+	ans += XlYl;
+	XrYr += XlYl;
+	auto XlXr = add(lhs_s, lhs_s + left_half, lhs_s + left_half, lhs_e);
+	auto YlYr = add(rhs_s, rhs_s + left_half, rhs_s + left_half, rhs_e);
+	auto mid_part=multiply_fast(XlXr, YlYr);
+	mid_part -= XrYr;
+	XlYl.li.assign(left_half, 0);
+	copy(begin(mid_part.li), end(mid_part.li), back_inserter(XlYl.li));
+	ans += XlYl;
+	return ans;
+}
+
+LargeInt multiply_fast(LargeInt& lhs, LargeInt& rhs)
+{
+	if (lhs.li.size() < rhs.li.size())
+		fill_n(back_inserter(lhs.li), rhs.li.size() - lhs.li.size(), 0);
+	else if (lhs.li.size() > rhs.li.size())
+		fill_n(back_inserter(rhs.li), lhs.li.size() - rhs.li.size(), 0);
+	return multiply_fast(begin(lhs.li), end(lhs.li), begin(rhs.li), end(rhs.li));
+}
+
+template<typename RandIter>
+LargeInt add(RandIter lhs_s, RandIter lhs_e, RandIter rhs_s, RandIter rhs_e)
+{
+	size_t left_size = distance(lhs_s, lhs_e);
+	size_t right_size = distance(rhs_s, rhs_e);
+	size_t size = max(left_size, right_size);
+	LargeInt ans(0, size + 1);
+	ans.li.clear();
+	if (left_size < right_size) {
+		copy(rhs_s, rhs_e, back_inserter(ans.li));
+		ans.add(lhs_s, lhs_e);
+	}
+	else {
+		copy(lhs_s, lhs_e, back_inserter(ans.li));
+		ans.add(rhs_s, rhs_e);
+	}
+	return ans;
+}
