eb18523e0e18ffa484858c1b7b8725d47df56374
WillX <willwsxu@gmail.com>
2018-11-12 23:22:09 -0600

    shipping toys

--- HackerRank/HackerRank.cpp
+++ HackerRank/HackerRank.cpp
@@ -575,3 +575,24 @@ TEST_CASE("Hacker rank Greedy luck balance", "[NEW]")
 	CHECK(luckBalance(3, vector<vector<int>>{ {5, 1}, { 2,1 }, { 1,1 }, { 8,1 }, { 10,0 }, { 5,0 }}) == 29);
 	CHECK(luckBalance(5, vector<vector<int>>{ {13, 1}, { 10,1 }, { 9,1 }, { 8,1 }, { 13,1 }, { 12,1 }, { 18,1 }, { 13,1 }}) == 42);
 }
+// toys of size <= smallest+4 can be shipped in one box
+// what is the minimal shipping
+int toys(vector<int> w) {
+	sort(begin(w), end(w));
+	auto cursor = begin(w);
+	int count = 0;
+	while (cursor != end(w)) {
+		int target = *cursor + 4;
+		cursor = lower_bound(cursor, end(w), target);
+		if (cursor != end(w)) {
+			if (*cursor == target)
+				++cursor;
+		}
+		count++;
+	}
+	return count;
+}
+TEST_CASE("Hacker rank Greedy toys shipping", "[NEW]")
+{
+	CHECK(toys(vector<int>{233, 960, 27, 817, 961, 919, 450, 19, 882, 278, 217, 304, 742, 920, 605, 92, 376, 64, 837, 31, 893, 273, 689, 188, 18, 334, 952, 807, 847, 296, 630, 80, 256, 657, 249, 570, 928, 699, 589, 811, 977, 806, 467, 71, 78, 72, 515, 806, 137, 352, 837, 382, 626, 879, 571, 996, 565, 875, 803, 412, 523, 785, 844, 780, 443, 93, 350, 723, 792, 291, 534, 121, 97, 353, 544, 527, 426, 412, 333, 915, 764, 523, 649, 742, 402, 572, 90, 319, 447, 246, 731, 323, 31, 927, 103, 826, 20, 805, 550, 164}) == 69);
+}
