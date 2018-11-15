b981c923b966b78788160f2d4853cc930d12bf9c
WillX <willwsxu@gmail.com>
2018-11-14 20:37:39 -0600

    refactor subarray min to use shared code

--- leetcode/stack.cpp
+++ leetcode/stack.cpp
@@ -161,25 +161,8 @@ public:
 	}
 	// 907. Sum of Subarray Minimums, sum of min of all sub array, n [1,30000]
 	int sumSubarrayMins(vector<int>& A) {
-		vector<int> prev_less_pos(A.size(), -1); //find  elem < current, on the left side
-		deque<int> plp{ 0 };  // monotonous none decreasing stack, allow equal
-		for (size_t i = 1; i < A.size(); i++) {
-			while (!plp.empty() && A[i] < A[plp.back()])  // don't cross equal value
-				plp.pop_back();
-			if (!plp.empty())
-				prev_less_pos[i] = plp.back();
-			plp.push_back(i);
-		}
-		plp.clear();
-		// do the same from back, effectively monotonously decreasing
-		vector<int> next_less_pos(A.size(), A.size()); // find elem < current, on the right side
-		for (int i = (int)A.size()-1; i >=0; i--) {  // i must be signed
-			while (!plp.empty() && A[i] <= A[plp.back()])  // cross equal value
-				plp.pop_back();
-			if (!plp.empty())
-				next_less_pos[i] = plp.back();
-			plp.push_back(i);
-		}
+		vector<int> prev_less_pos = computePrevLess(A, less<int>());// from L to R, maintain increasing stack, allow equal
+		vector<int> next_less_pos = computeNextLess(A, less_equal<int>());// from R to L, maintain increasing stack
 		int MOD = 1000000007;
 		long long sum = 0;
 		for (size_t i = 0; i < A.size(); i++) {
@@ -246,11 +229,11 @@ public:
 			}
 		}
 		int max_area = 0;
-		// each row is be base of a histogram, like problem #84
+		// each row is the base of a histogram, like problem #84
 		for (int r = 0; r < rows; r++) {
 			vector<int> prev_less_pos = computePrevLess(heights[r], less_equal<int>());// from L to R, maintain increasing stack
 			vector<int> next_less_pos = computeNextLess(heights[r], less_equal<int>());// from R to L, maintain increasing stack
-			for (size_t c = 0; c < cols; c++)  // height of current bar will be height of the rentangle
+			for (int c = 0; c < cols; c++)  // height of current bar will be height of the rentangle
 				max_area = max(max_area, heights[r][c] * (next_less_pos[c] - prev_less_pos[c] - 1));
 		}
 		return max_area;
