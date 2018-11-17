b179a396d79a4b1df313d0c519510450f2cde06b
WillX <willwsxu@gmail.com>
2018-11-16 20:23:40 -0600

    fix size bug in unionFind

--- library/UnionFind.h
+++ library/UnionFind.h
@@ -3,20 +3,24 @@
 #include <utility>
 #include <algorithm>
 
+
 class UnionFind
 {
 	std::vector<int> parents;
 	std::vector<int> size;
-
+	int        nodes_added = 0;
 public:
 	UnionFind(int N) : parents(N + 1, -1), size(N + 1, 1) {}
 	int find(int u) {
-		if (parents[u] < 0)
+		if (parents[u] < 0) {
 			return u;
+		}
 		return find(parents[u]);
 	}
 	bool union_(int u, int v)
 	{
+		update_count(u);
+		update_count(v);
 		int pu = find(u);
 		int pv = find(v);
 		if (pu == pv)  // find cycle
@@ -24,7 +28,26 @@ public:
 		if (size[pu] > size[pv])
 			std::swap(pu, pv);
 		parents[pu] = pv;   // extend longer as it is currently shorter
-		size[pu] += size[pv];
+		size[pv] += size[pu];
 		return true;
+	}
+	int get_size(int u)
+	{
+		return size[find(u)] == 1 ? 0 : size[find(u)];
+	}
+	int get_count() const {
+		return nodes_added;
+	}
+	void update_count(int u) {
+		if (parents[u] >= 0 || size[u] > 1)
+			return;
+		nodes_added++;
+	}
+	int get_count(set<int>& bad) const {
+		for (int u : bad) {
+			if (parents[u] >= 0 || size[u] > 1)
+				bad.erase(u);
+		}
+		return nodes_added + bad.size();
 	}
 };
