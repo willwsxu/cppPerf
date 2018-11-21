ff043fccb3ecd24f024724c3ea8fd80864c57688
WillX <willwsxu@gmail.com>
2018-11-20 20:55:04 -0600

    ftoa _fix version benchmark

--- bmtests/bm_performance_general/bm_itoa.cpp
+++ bmtests/bm_performance_general/bm_itoa.cpp
@@ -172,7 +172,7 @@ static void BM_itoa_fix(benchmark::State& state)  // 7.3 ns vs 63.8 ns experimen
 		benchmark::DoNotOptimize(val = itoa_fix(-1234567890, szValue, szValue + state.range(0)));
 	}
 }
-BENCHMARK(BM_itoa_fix)->Arg(12)->Arg(14)->Arg(16)->Arg(16);
+BENCHMARK(BM_itoa_fix)->Arg(12)->Arg(14)->Arg(16)->Arg(18);
 
 static void BM_itoa_fix_experiment(benchmark::State& state)
 {
@@ -183,8 +183,40 @@ static void BM_itoa_fix_experiment(benchmark::State& state)
 		benchmark::DoNotOptimize(val = itoa_fix_experiment(-1234567890, szValue, szValue + state.range(0)));
 	}
 }
-BENCHMARK(BM_itoa_fix_experiment)->Arg(12)->Arg(14)->Arg(16)->Arg(16);
+BENCHMARK(BM_itoa_fix_experiment)->Arg(12)->Arg(14)->Arg(16)->Arg(18);
 
+void itoa_fill(int val, char *pBack, int len)
+{
+	while (len--) {
+		*pBack-- = val % 10 + '0';
+		val /= 10;
+	}
+}
+bool ftoa_fix(int64_t val, unsigned short dp, char *szValue, char *pEnd)
+{
+	if (dp >= 10)
+		return false;
+	const static int pow10_[10]={1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+	if (val < 0) {
+		*szValue++ = '-';
+		val = -val;
+	}
+	itoa_fill(val%pow10_[dp], pEnd - 1, dp);
+	pEnd -= dp+1;
+	*pEnd-- = '.';
+	itoa_fill(val/pow10_[dp], pEnd, pEnd- szValue+1);
+	return true;
+}
+static void BM_ftoa_fix(benchmark::State& state)
+{
+	char szValue[20] = { 0 };
+	bool success = false;
+	while (state.KeepRunning())
+	{
+		benchmark::DoNotOptimize(success = ftoa_fix(-1234567890, 2, szValue, szValue+state.range(0)));
+	}
+}
+BENCHMARK(BM_ftoa_fix)->Arg(12)->Arg(14)->Arg(16)->Arg(18);
 
 char* ftoa2(int val, int dp, char *szValue, int size)
 {
