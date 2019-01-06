b065f89931e92f1ca670571d9bdb70a39f885189
will x360 <willwsxu@gmail.com>
2019-01-05 21:24:52 -0600

    euler phi min quotient optimization

--- library/ProjectEulerOptimize.cpp
+++ library/ProjectEulerOptimize.cpp
@@ -25,7 +25,7 @@ int spiral_prime_diag(int pct)  // below pct%
 	}
 	return 0;
 }
-TEST_CASE("Project Euler #58: Spiral primes", "[NEW]")
+TEST_CASE("Project Euler #58: Spiral primes", "[LARGE]")
 {
 	CHECK(spiral_prime_diag(60) == 5);
 	CHECK(spiral_prime_diag(40) == 31);
@@ -35,4 +35,52 @@ TEST_CASE("Project Euler #58: Spiral primes", "[NEW]")
 	CHECK(spiral_prime_diag(11) == 12201);
 	CHECK(spiral_prime_diag(9) == 74373);
 	CHECK(spiral_prime_diag(8) == 238733);
+}
+
+// special phi, only ingterested in find phi when n/phi is <= previous min
+int euler_phi_min_q(int n, const vector<int>& primes, double min_q) { //phi(n)=n*(1-1/p)(1-1/q)...
+																//auto factors=prime_factors(n, primes);
+	int phi = n;
+	int n_reduce = n;
+	for (int f : primes) {
+		if (f*f>n_reduce)
+			break;
+		if (n_reduce%f != 0)
+			continue;
+		while (n_reduce%f == 0)
+			n_reduce /= f;
+		phi -= phi / f;
+		if (phi*min_q<n)  // n/phi > min_q
+			break;
+	}
+	if (n_reduce>1)
+		phi -= phi / n_reduce;
+	return phi;
+}
+// two critical optimizations to pass largest test
+// euler_phi_min_q optimized to break out loop if n/phi is larger
+//   also don't calculator prime factors early, do it while calculating phi, to take advantge of the optmization above
+// call get_largest_permu after n/phi is not worse
+int totient_permutation_min(int N) // 
+{
+	double ratio = 99999999;
+	auto primes = make_primes((int)sqrt(N));
+	int ans = 0;
+	for (int n = N - 1; n>1; n--) {
+		auto phi = euler_phi_min_q(n, primes, ratio);
+		double new_ratio = (double)n / phi;
+		//cout << n << " phi=" << phi.first << " ratio =" << new_ratio << "\n";
+		if (new_ratio > ratio)
+			continue;
+		if (get_largest_permu(n) == get_largest_permu(phi)) { // n and phi are permutation
+			ans = n;
+			ratio = new_ratio;
+		}
+	}
+	return ans;
+}
+TEST_CASE("Project Euler #70: Totient permutation", "[LARGE]")
+{
+	CHECK(totient_permutation_min(1000000) == 783169);
+	CHECK(totient_permutation_min(10000000) == 8319823);
 }
