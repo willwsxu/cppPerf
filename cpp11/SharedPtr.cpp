3a33801f9f97532092502409e316a2cf6f07c965
will x360 <willwsxu@gmail.com>
2019-02-09 10:08:03 -0600

    shared pointer delete

--- cpp11/SharedPtr.cpp
+++ cpp11/SharedPtr.cpp
@@ -14,10 +14,16 @@ public:
 		if (naked)
 			counter = new int(1);
 	}
+	~SharedPtr() noexcept
+	{
+		delete_me();
+	}
 	SharedPtr(const SharedPtr& other) noexcept {
-		naked = other.naked;
-		counter = other.counter;
-		(*counter)++;
+		if (other.naked) {
+			naked = other.naked;
+			counter = other.counter;
+			(*counter)++;
+		}
 	}
 	SharedPtr(SharedPtr&& other) noexcept {
 		naked = other.naked;
@@ -25,12 +31,45 @@ public:
 		other.naked = nullptr;  // old pointer must now be empty
 		other.counter = nullptr;
 	}
+	SharedPtr& operator=(const SharedPtr& other) noexcept
+	{
+		if (*this != other) {  // not self
+			if (naked == other.naked) { //  same pointer
+				if (naked)  // make sure pointer is valid
+					(*counter)++;
+			}
+			else {
+				delete_me();
+				copy(other);
+			}
+		}
+		return *this;
+	}
+	SharedPtr& operator=(SharedPtr&& other) noexcept
+	{
+		if (*this != other) {  // not self
+			if (naked == other.naked) { //  same pointer
+				if (naked)  // make sure pointer is valid
+					(*counter)--;
+			}
+			else {
+				delete_me();
+				naked = other.naked;
+				counter = other.counter;
+			}
+			other.naked == nullptr;
+			other.counter = nullptr;
+		}
+		return *this;
+	}
+
 	template<typename Y>
 	SharedPtr(const SharedPtr<Y>& other) noexcept {  // why this is discarded by compiler
 		naked = other.naked;
 		counter = other.counter;
 		(*counter)++;
 	}
+
 	T& operator*() const noexcept {
 		return *naked;
 	}
@@ -39,6 +78,26 @@ public:
 	}
 	int use_count() const {
 		return counter == nullptr ? 0: *counter;
+	}
+private:
+	void delete_me() {
+		if (naked) {
+			if (*counter == 1) {
+				delete naked;
+				delete counter;
+			}
+			else
+				(*counter)--;
+			counter = nullptr;
+			naked = nullptr;
+		}
+	}	
+	void copy(const SharedPtr& other) noexcept {
+		naked = other.naked;
+		counter = other.counter;
+		if (other.naked) {
+			(*counter)++;
+		}
 	}
 };
 
@@ -52,6 +111,10 @@ TEST_CASE("Jump Shared Ptr test", "[TEST]")
 		long long *x = nullptr;
 		SharedPtr<long long> empty3(x);
 		CHECK(empty3.use_count() == 0);
+		SharedPtr<long long> empty4=empty3;  // copy
+		CHECK(empty4.use_count() == 0);
+		SharedPtr<long long> empty5 = move(empty3);  // move
+		CHECK(empty5.use_count() == 0);
 
 		shared_ptr<long long> std_empty;
 		CHECK(std_empty.use_count() == 0);
@@ -60,19 +123,33 @@ TEST_CASE("Jump Shared Ptr test", "[TEST]")
 		shared_ptr<long long> std_empty3(x);  // ownership of nullptr
 		CHECK(std_empty3.use_count() == 1);   // std seems wrong
 	}
-	SharedPtr<long long> long_int1(new long long(2));
-	CHECK(long_int1.use_count() == 1);
 
-	SharedPtr<long long> long_int2 = long_int1;
-	CHECK(long_int1.use_count() == 2);
-	CHECK(long_int2.use_count() == 2);
+	SECTION("test constructor and destructor") {
+		SharedPtr<long long> long_int1(new long long(2));
+		CHECK(long_int1.use_count() == 1);
 
-	SharedPtr<long long> long_int3 = long_int1;
-	CHECK(long_int1.use_count() == long_int2.use_count());
-	CHECK(long_int3.use_count() == 3);
+		SharedPtr<long long> long_int2 = long_int1;
+		CHECK(long_int1.use_count() == 2);
+		CHECK(long_int2.use_count() == 2);
 
-	CHECK(*long_int3 == 2);
-	*long_int3 = 3;
-	CHECK(*long_int2 == 3);
+		SharedPtr<long long> long_int3 = long_int1;
+		CHECK(long_int1.use_count() == long_int2.use_count());
+		CHECK(long_int3.use_count() == 3);
+
+		CHECK(*long_int3 == 2);
+		*long_int3 = 3;
+		CHECK(*long_int2 == 3);
+
+		long_int1.~SharedPtr();
+		CHECK(long_int1.use_count() == 0);
+		CHECK(long_int2.use_count() == 2);
+
+		long_int2.~SharedPtr();
+		CHECK(long_int2.use_count() == 0);
+		CHECK(long_int3.use_count() == 1);
+
+		long_int3.~SharedPtr();
+		CHECK(long_int3.use_count() == 0);
+	}
 
 }
