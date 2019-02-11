c633215391226c1b9e75c6390dbc017c657cf55e
will x360 <willwsxu@gmail.com>
2019-02-10 22:12:09 -0600

    data feed regional book

---
+++ cpp11/DataFeed.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+vector<string> get_tokens(string&& data)
+{
+	istringstream strm(move(data));
+	vector<string> fields;
+	for (string token; getline(strm, token, '|'); ) {
+		fields.push_back(token);
+	}
+	return fields;
+}
+
+class RegionalBook
+{
+	struct Quote
+	{
+		int bid;
+		int ask;
+		Quote(int b=0, int a=0) : bid(b), ask(a) {}
+	};
+	map<string, map<string, Quote>> book;
+public:
+	void update_message(string&& msg) {
+		auto parsed = get_tokens(move(msg));
+		if (parsed[0] != "Q")
+			return;
+		book[parsed[2]][parsed[1]] = Quote(stoi(parsed[3]), stoi(parsed[4]));
+	}
+	void display()
+	{
+		for (const auto& sym : book) {
+			cout << "Symbol: " << sym.first << "\n";
+			map<int, string, greater<int>> bids;
+			map<int, string> asks;
+			for (const auto& ex : sym.second) {
+				bids[ex.second.bid] = ex.first;
+				asks[ex.second.ask] = ex.first;
+			}
+			for (const auto& bid : bids)
+				cout << bid.second << " " << bid.first << "\n";
+			for (const auto& ask : asks)
+				cout << ask.second << " " << ask.first << "\n";
+		}
+	}
+};
+
+#include "..\catch.hpp"
+TEST_CASE("Peak6 data feed test", "[TEST]")
+{
+	RegionalBook book;
+	book.update_message("Q|NYS|IBM|99|105|");
+	book.update_message("Q|NAS|IBM|98|102|");
+	book.update_message("Q|ARC|IBM|101|103|");
+	book.display();
+}
