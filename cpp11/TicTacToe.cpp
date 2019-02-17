41db71ad824218240ec504045edacff92536cae6
will x360 <willwsxu@gmail.com>
2019-02-16 20:58:33 -0600

    Create TicTacToe.cpp

---
+++ cpp11/TicTacToe.cpp
@@ -0,0 +1,104 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class TicTacToeBoard
+{
+	enum Piece { PLAYER = 'X', COMPUTER = 'O' };
+	static const int DIMENSION = 3;
+	vector<char> board;
+public:
+	TicTacToeBoard() :board(DIMENSION*DIMENSION, ' ') {}
+	void set_move(char piece, int row, int col) {
+		board[row*DIMENSION + col] = piece;
+	}
+	bool row_won(int r) {
+		r *= DIMENSION;
+		return board[r + 1] = board[r] && board[r + 2] == board[r];
+	}
+	bool col_won(int c) {
+		return board[c + 3] == board[c] && board[c + 6] == board[c];
+	}
+	bool forward_diag_won()  // 2 4 6
+	{
+		return board[2] == board[4] && board[2] == board[6];
+	}
+	bool backward_diag_won()  // 0 4 8
+	{
+		return board[0] == board[4] && board[0] == board[8];
+	}
+	bool game_over() const {
+		return find(begin(board), end(board), ' ') == end(board);
+	}
+	bool is_blank(int r, int c) const {
+		return board[r*DIMENSION + c] == ' ';
+	}
+	int evaluate() {  // PLAYER won 10, or -10 if computer won
+		auto score = [](char side) { return side == PLAYER ? 10 : -10; };
+		for (int i = 0; i<DIMENSION; i++) {
+			if (row_won(i))
+				return score(board[i * 3]);
+			if (col_won(i))
+				return score(board[i]);
+		}
+		if (forward_diag_won() || backward_diag_won())
+			return score(board[4]);
+		return 0;
+	}
+	int minimax(int depth, bool isMax, int alpha, int beta)
+	{
+		int score = evaluate();
+		if (score == 10)
+			return score - depth;
+		else if (score == -10)
+			return score + depth;
+		if (game_over())
+			return 0;
+		// compute score of next move
+		score = isMax ? -1000 : 1000;
+		for (int r = 0; r<DIMENSION; r++) {
+			for (int c = 0; c<DIMENSION; c++) {
+				if (!is_blank(r, c))
+					continue;
+				set_move(isMax ? PLAYER : COMPUTER, r, c);
+				int next_score = minimax(depth + 1, !isMax, alpha, beta);
+				score = isMax ? max(score, next_score) : min(score, next_score);
+				set_move(' ', r, c); // roll back
+				if (isMax)
+					alpha = max(alpha, score);
+				else
+					beta = min(score, beta);
+				if (beta <= alpha) {
+					break;
+				}
+			}
+		}
+		return score;
+	}
+	pair<int, int> find_best_move(bool isMax)
+	{
+		int score = isMax ? -10000 : 10000;
+		pair<int, int> best;
+
+		for (int r = 0; r<DIMENSION; r++) {
+			for (int c = 0; c<DIMENSION; c++) {
+				if (!is_blank(r, c))
+					continue;
+				set_move(isMax ? PLAYER : COMPUTER, r, c);
+				int next_score = minimax(0, !isMax, -10000, 10000);
+				set_move(' ', r, c); // roll back
+				if (isMax && next_score>score || !isMax && next_score < score)
+					score = next_score;
+				best.first = r;
+				best.second = c;
+			}
+		}
+		return best;
+	}
+};
+
+
+#include "..\catch.hpp"
+TEST_CASE("Tic Tac Toe test", "[TEST]")
+{
+}
