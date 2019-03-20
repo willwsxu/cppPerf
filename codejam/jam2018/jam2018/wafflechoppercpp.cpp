23d7e412a51b927bb44481c9a89903a288064c9d
Will XU <willwsxu@gmail.com>
2019-03-19 22:32:11 -0500

    speed up waffle chopper to solve TLE

--- codejam/jam2018/jam2018/wafflechoppercpp.cpp
+++ codejam/jam2018/jam2018/wafflechoppercpp.cpp
@@ -15,45 +15,14 @@ int count_chips(const vector<string>& grid, int rs, int re, int cs, int ce)  // 
 		chips += std::count(begin(grid[rs])+cs, begin(grid[rs])+ce, '@');
 	return chips;
 }
-// first cut, piece at top left, RxC dim
-// H and V are total cuts needed
-bool first_cut(const vector<string>& grid, int R, int C, int H, int V, int chips)
-{
-	if (count_chips(grid, 0, R, 0, C) != chips)
-		return false;
-	vector<int> rows{0, R };// find horizontal cuts, including 0 for convenience in step 2
-	for (int r = R+1; r <= (int)grid.size(); r++) {
-		int count = count_chips(grid, rows.back(), r, 0, C);
-		if (count == chips)
-			rows.push_back(r);
-		else if (count > chips)
-			return false;
-	}
-	if (rows.size() != H + 2)
-		return false;
-	// step 2: from col 1, find cuts that produce good # of chips in each row cut
-	vector<int> cols{ C };
-	for (int c = C + 1; c <= (int)grid[0].size(); c++) {
-		int good = 0;
-		for (int i = 0; i < (int)rows.size() - 1; i++) {
-			int count = count_chips(grid, rows[i], rows[i+1], cols.back(), c);
-			if (count == chips)
-				good++;
-			else if (count > chips)
-				return false;
-		}
-		if (good == H+1)
-			cols.push_back(c);
-	}
-	return cols.size() == V + 1;
-}
+// ffor valid row cuts, search valid col cut one at a time
 bool cut_vertical(const vector<string>& grid, const vector<int>& h_cuts, int v_cut, const int V, const int chips)
 {
-	if (V<0)
+	if (V<0)  // decrement V each call until -1
 		return true;
-	int c = 0 == V ? grid[0].size() : v_cut + 1;  // speal case for last cut
+	int c = 0 == V ? grid[0].size() : v_cut + 1;  // special case for last cut, use all remaining columns
 	for (; c <= (int)grid[0].size(); c++) {
-		int good = 0;
+		int good = 0;  // check row cut by row cut for this column
 		for (int i = 0; i < (int)h_cuts.size() - 1; i++) {
 			int count = count_chips(grid, h_cuts[i], h_cuts[i + 1], v_cut, c);
 			if (count == chips)
@@ -76,9 +45,15 @@ bool cut_horizonal(const vector<string>& grid, vector<int>& h_cuts, const int H,
 		h_cuts.pop_back();
 		return ans;
 	}
+	int row_start = h_cuts.back();
 	int r = h_cuts.back() + 1;
 	h_cuts.push_back(r);
 	for (; r <= int(grid.size() - H + h_cuts.size() - 1); r++) {
+		int count = count_chips(grid, row_start, r, 0, grid[0].size()); // count chips per horizontal cut to prune invalid
+		if (count > chips*(V + 1)) // too many chips
+			break;
+		else if (count < chips*(V + 1))  // not enough chips
+			continue;
 		h_cuts.back() = r;
 		if (cut_horizonal(grid, h_cuts, H, V, chips))
 			return true;
@@ -86,6 +61,7 @@ bool cut_horizonal(const vector<string>& grid, vector<int>& h_cuts, const int H,
 	h_cuts.pop_back();
 	return false;
 }
+
 bool pancake_cuts(const vector<string>& grid, int H, int V) {
 	int total_chips = 0;
 	for (const auto& row : grid)
@@ -93,15 +69,6 @@ bool pancake_cuts(const vector<string>& grid, int H, int V) {
 	int chips = total_chips / (H + 1) / (V + 1);
 	if (chips*(H + 1)*(V + 1) != total_chips)
 		return false;
-	/*
-	for (int i = 1; i <= (int)grid.size() - H; i++) {
-		for (int j = 1; j <= (int)grid[0].size() - V; j++) {
-			if (first_cut(grid, i, j, H, V, chips))
-				return true;
-		}
-	}
-	return false;
-	*/
 	vector<int> h_cuts{0};  // [0,1,2] example for 1 cut 2 rows
 	return cut_horizonal(grid, h_cuts, H, V, chips);
 }
