986bf415cba77d86021688f9a6410512cea1147c
Will XU <willwsxu@gmail.com>
2019-05-16 07:44:24 -0500

    triangulation fix to pass TLE

--- leetcode/dynamicProgramming/triangulation.cpp
+++ leetcode/dynamicProgramming/triangulation.cpp
@@ -33,16 +33,38 @@ int minScoreTriangulation(vector<int>& points, map<vi, int>& memo)
 	}
 	return result;
 }
+
+int minScoreTriangulation(vector<int>& points, vector<vector<int>>& memo, int i, int j)
+{
+	if (j-i<2)
+		return 0;
+	if (memo[i][j] > 0)
+		return memo[i][j];
+	int result = INT32_MAX;
+	// split a polygon into 2 valid polygons
+	for (int k = i+1; k < j; k++) {  // # of valid starting place
+		int middle = points[i] * points[j] * points[k];
+		result = min(result, minScoreTriangulation(points, memo,i,k) +middle+ minScoreTriangulation(points, memo, k,j));
+	}
+	memo[i][j] = result;
+	return result;
+}
+
 int minScoreTriangulation(vector<int>& points)
 {
-	map<vi, int> memo;
-	return minScoreTriangulation(points, memo);
+	//map<vi, int> memo;
+	//return minScoreTriangulation(points, memo);
+	size_t N = points.size();
+	vector<vector<int>> memo(N, vector<int>(N,0));
+	return minScoreTriangulation(points, memo, 0, N - 1);
 }
 
 #include "catch.hpp"
 TEST_CASE("jam2019 1B #3", "[1B3]")
 {
-	CHECK(minScoreTriangulation(vector<int>{2, 1, 4, 4}) == 24);
+	CHECK(minScoreTriangulation(vector<int>{2, 7, 1, 9, 3, 1, 2, 2, 9, 2, 7, 1, 9, 3, 1, 2, 2, 9, 2, 7, 1, 9, 3, 1, 2, 2, 9, 2, 7, 1, 9, 3, 1, 2, 2, 9, 2, 7, 1, 9, 3, 1, 2, 2, 9, 2, 7, 1, 9, 3}) == 488);
+	CHECK(minScoreTriangulation(vector<int>{2, 7, 1, 9, 3, 1, 2, 2, 9}) == 86);
+	CHECK(minScoreTriangulation(vector<int>{2, 1, 4, 4}) == 24);  // fix loop count issue
 	CHECK(minScoreTriangulation(vector<int>{3,7,4,5}) == 144);
 	CHECK(minScoreTriangulation(vector<int>{1, 3, 1, 4, 1, 5}) == 13);
 }
