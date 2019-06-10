473aa03dd3c4f7235a263204486513562cac450b
Will XU <willwsxu@gmail.com>
2019-06-09 22:32:54 -0500

    tree insufficinet node, fail some tests

---
+++ leetcode/tree/binary_tree.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int sufficientSubset_dfs(TreeNode* root, int limit) {
+    if (!root)
+        return 0;
+    int left = sufficientSubset_dfs(root->left, limit);
+    int right = sufficientSubset_dfs(root->right, limit);
+    if (root->left && left < limit) {
+        root->left = nullptr;
+        left = 0;
+    }
+    if (root->right && right < limit) {
+        root->right = nullptr;
+        right = 0;
+    }
+    int val = 0;
+    if (root->left && root->right)
+        val = min(left, right);
+    else if (root->left)
+        val = left;
+    else if (root->right)
+        val = right;
+    return root->val + val;
+}
+TreeNode* sufficientSubset(TreeNode* root, int limit) {
+    int val = sufficientSubset_dfs(root, limit);
+    return val < limit ? nullptr : root;
+}
