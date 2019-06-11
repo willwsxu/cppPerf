3aeab565218a7b1bd40715f593be73034100f73b
Will XU <willwsxu@gmail.com>
2019-06-11 18:10:32 -0500

    cmake function, pre build dependency

--- leetcode/compilerOptions.cmake
+++ leetcode/compilerOptions.cmake
@@ -8,3 +8,19 @@ foreach (_varName ${_varNames})
         message(STATUS "${_varName}=${${_varName}}")
     endif()
 endforeach()
+
+function(update_CXX_flag proj_name warning_level debug_info)
+
+if (MSVC)
+    # remove default debug info from CMAKE_CXX_FLAGS_INIT
+    string (REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS_INIT ${CMAKE_CXX_FLAGS_INIT})
+    string (REGEX REPLACE "/Zi" "" CMAKE_CXX_FLAGS_DEBUG_INIT ${CMAKE_CXX_FLAGS_DEBUG_INIT})
+
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_INIT} ${warning_level}")
+	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG_INIT} ${debug_info}")
+endif()
+Message(STATUS "CMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}")
+Message(STATUS "CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}")
+#target_compile_options(leetcode PRIVATE $<$<CONFIG:Debug>:/ZI> $<$<CONFIG:Release>:/Zi>)  // did not replace Zi with ZI
+target_link_options(${proj_name} PRIVATE $<$<CONFIG:Release>:/DEBUG /OPT:REF /OPT:ICF>)
+endfunction()
---
+++ leetcode/pre_built_command2.cmake
@@ -0,0 +1,2 @@
+
+Message(STATUS "call pre build 2 from ${CMAKE_CURRENT_LIST_DIR}")
