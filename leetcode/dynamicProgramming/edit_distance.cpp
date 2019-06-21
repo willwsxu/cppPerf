3e56576042e3cf4d4c4ac8627fa442a26d6d5708
Will XU <willwsxu@gmail.com>
2019-06-20 21:36:27 -0500

    move over LCS problem

--- leetcode/dynamicProgramming/edit_distance.cpp
+++ leetcode/dynamicProgramming/edit_distance.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 string shortestSuperseq_dp(string& str1, string& str2, int i, int j, vector<vector<string>>& memo)
@@ -26,8 +27,38 @@ string shortestCommonSupersequence(string str1, string str2) {
     return shortestSuperseq_dp(str1, str2, 0, 0, memo);
 }
 
+
+// 960. Delete Columns to Make Sorted III
+int minDeletionSize(vector<string>& A) {
+    int cols = A[0].size();
+    vector<int> lcs(cols, 1);  // longest increasing sequence for all rows
+    for (int c = 1; c < cols; c++) {  // compute LCS ends at c
+        for (int i = 0; i < c; i++) {
+            int increasing = 1;
+            for (int r = 0; r < (int)A.size(); r++) {
+                if (A[r][i] > A[r][c]) {
+                    increasing = 0;
+                    break;
+                }
+            }
+            if (increasing) {
+                lcs[c] = max(lcs[c], lcs[i] + increasing);
+            }
+        }
+    }
+    return cols - *max_element(begin(lcs), end(lcs));
+}
 #include "catch.hpp"
 TEST_CASE("1092. Shortest Common Supersequence", "[DP]")
 {
     CHECK(shortestCommonSupersequence("abac", "cab") == "cabac");
+}
+
+TEST_CASE("960. Delete Columns to Make Sorted III", "[DP]")
+{
+    CHECK(minDeletionSize(vector<string>{"abbbaa"}) == 2);
+    CHECK(minDeletionSize(vector<string>{"abbba"}) == 1);
+    CHECK(minDeletionSize(vector<string>{"babca", "bbazb"}) == 3);
+    CHECK(minDeletionSize(vector<string>{"edcba"}) == 4);
+    CHECK(minDeletionSize(vector<string>{"ghi", "def", "abc"}) == 0);
 }
