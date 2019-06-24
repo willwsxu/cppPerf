057923d3a0361dfa9c58cb5a7ee3f29407ff2184
Will XU <willwsxu@gmail.com>
2019-06-23 20:41:58 -0500

    iterative binary search, use less memory

--- leetcode/binary_search/MountainArray.cpp
+++ leetcode/binary_search/MountainArray.cpp
@@ -56,21 +56,16 @@ class MountainArrayQuery {
             return find_top(mountainArr, low, mid - 1);
         return mid;
     }
+    // iterative binary search
     int find_top_loop(MountainArray& mountainArr, int low, int high) {
         while (low < high) {
-            if (high - low == 1) {
-                if (mountain[high] > mountain[low])
-                    return high;
-                return low;
-            }
             int mid = (low + high) / 2;
             query(mountainArr, mid);
-            query(mountainArr, mid - 1);
             query(mountainArr, mid + 1);
-            if (mountain[mid - 1] < mountain[mid] && mountain[mid] < mountain[mid + 1])
+            if (mountain[mid] < mountain[mid + 1])  // low to mid+1 increasing
                 low = mid + 1;
-            if (mountain[mid - 1] > mountain[mid] && mountain[mid] > mountain[mid + 1])
-                high = mid - 1;
+            if (mountain[mid] > mountain[mid + 1])  // mid to high decreasing
+                high = mid;
         }
         return low;
     }
@@ -85,6 +80,19 @@ class MountainArrayQuery {
             return findInMountainArrayInc(target, mountainArr, mid + 1, high);
         return findInMountainArrayInc(target, mountainArr, low, mid - 1);
     }
+    int findInMountainArrayInc_loop(int target, MountainArray& mountainArr, int low, int high) {  // inclusive
+        while (low<=high) {
+            int mid = (low + high) / 2;
+            query(mountainArr, mid);
+            if (target == mountain[mid])
+                return mid;
+            if (target > mountain[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
     int findInMountainArrayDec(int target, MountainArray& mountainArr, int low, int high) {  // inclusive
         if (low >= high)
             return low;
@@ -96,6 +104,19 @@ class MountainArrayQuery {
             return findInMountainArrayDec(target, mountainArr, mid + 1, high);
         return findInMountainArrayDec(target, mountainArr, low, mid - 1);
     }
+    int findInMountainArrayDec_loop(int target, MountainArray& mountainArr, int low, int high) {  // inclusive
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            query(mountainArr, mid);
+            if (target == mountain[mid])
+                return mid;
+            if (target < mountain[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
     vector<int> mountain;
 public:
     int findInMountainArray(int target, MountainArray& mountainArr) {
@@ -103,15 +124,10 @@ public:
         mountain.resize(size);
         fill(begin(mountain), end(mountain), -1);
         int top = find_top_loop(mountainArr, 0, size - 1);
-        int found = findInMountainArrayInc(target, mountainArr, 0, top);
-        query(mountainArr, found);
-        if (mountain[found] == target)
-            return found;
-        found = findInMountainArrayDec(target, mountainArr, top + 1, size - 1);
-        query(mountainArr, found);
-        if (mountain[found] == target)
-            return found;
-        return -1;
+        int found = findInMountainArrayInc_loop(target, mountainArr, 0, top);
+        if (found<0)
+            found = findInMountainArrayDec_loop(target, mountainArr, top + 1, size - 1);
+        return found;
     }
 };
 #include <catch.hpp>
