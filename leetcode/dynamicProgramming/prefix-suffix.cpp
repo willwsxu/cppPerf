d1415673ad640692182c954a6a62f3688816a084
Will XU <willwsxu@gmail.com>
2019-06-30 10:16:45 -0500

    bottom up dp

--- leetcode/dynamicProgramming/prefix-suffix.cpp
+++ leetcode/dynamicProgramming/prefix-suffix.cpp
@@ -20,14 +20,33 @@ int minHeightShelves_dp(vector<vector<int>>& books, int shelf_width, int idx, ve
     memo[idx] = result;
     return result;
 }
-int minHeightShelves(vector<vector<int>>& books, int shelf_width) {
+int minHeightShelves_topdown(vector<vector<int>>& books, int shelf_width) {
     vector<int> memo(books.size(), 0);
     return minHeightShelves_dp(books, shelf_width, 0, memo);
 }
 
 
+int minHeightShelves(vector<vector<int>>& books, int shelf_width) {
+    vector<int> memo(books.size()+1, 0);
+    for (int i = 0; i < books.size(); i++) {
+        int height = 0;
+        int thickness = 0;
+        int result = 1000000000;
+        for (int j = i; j >= 0; j--) {
+            thickness += books[j][0];
+            if (thickness > shelf_width)
+                break;
+            height = max(books[j][1], height);
+            result = min(height + memo[j], result);
+        }
+        memo[i + 1] = result;
+    }
+    return memo[books.size()];
+}
+
 #include <catch.hpp>
 TEST_CASE("1105. Filling Bookcase Shelves", "[DP]")
 {
     CHECK(minHeightShelves(vector<vector<int>>{ {1, 1}, { 2, 3 }, { 2, 3 }, { 1, 1 }, { 1, 1 }, { 1, 1 }, { 1, 2 }}, 4) == 6);
+    CHECK(minHeightShelves_topdown(vector<vector<int>>{ {1, 1}, { 2, 3 }, { 2, 3 }, { 1, 1 }, { 1, 1 }, { 1, 1 }, { 1, 2 }}, 4) == 6);
 }
