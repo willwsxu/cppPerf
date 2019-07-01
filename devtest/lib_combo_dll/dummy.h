6885829c7475cccdb3619f5950f3dc114aef525b
will xu <weisong.xu@tradingtechnologies.com>
2019-07-01 14:35:24 -0500

    generate export for dll to compile

--- devtest/lib_combo_dll/dummy.h
+++ devtest/lib_combo_dll/dummy.h
@@ -1,4 +1,8 @@
 #include <string>
+// dll symbols are hidden unless WINDOWS_EXPORT_ALL_SYMBOLS=ON
+// generate_export_header is needed to produce export header before compile
+
+#include "libcombo_dll_export.h"
 
 void dummy();
 class DummyClass
