2fdc901b90d165604654203ca2fd9ccb5c2e2fb0
Will XU <willwsxu@gmail.com>
2019-07-02 21:16:49 -0500

    inner class represent string window

--- leetcode/binary_search/sliding_window.cpp
+++ leetcode/binary_search/sliding_window.cpp
@@ -6,26 +6,39 @@
 using namespace std;
 class LongestDupSubStr
 {
-    const static int MOD = 1000000007;
+    const static long MOD = 10000000000000007;
     vector<long long> pow31;
     vector<long long> poly_sum;  // pre-compute polynomial sum, e.g. 26 chars: a*31^25+b*31^24+...+y*31+z
-    // finding dup string of length L, using hash
-    int longestDupSubstring_helper(const string& s, int L)
+public:
+    class string_window_view
     {
-        if (L == 0)
-            return -1;
-        unordered_set<long> substr_hash;
-        substr_hash.reserve((s.size() - L) * 2);
-        for (int right = L-1; right < s.size(); right++) {  // string s window [left, right] 
-            int left_char = right - L + 1;  // correspond to poly_sum window [left+1, right+1]
-            long long left_sum = poly_sum[left_char]*pow31[L]%MOD;
-            long long window_hash = (poly_sum[right+1]- left_sum+MOD)%MOD;
-            if (substr_hash.count(window_hash))
-                return right;
-            substr_hash.insert(window_hash);
+        const string* data;
+        int left, right;
+        long hash_val;
+        bool data_equal(const LongestDupSubStr::string_window_view& rhs) const {
+            for (int i = left, j = rhs.left; i <= right; i++, j++) {
+                if ((*data)[i] != (*rhs.data)[j])
+                    return false;
+            }
+            return true;
         }
-        return -1;
-    }
+    public:
+        string_window_view(const string& d, int l, int r, const vector<long long>& pow31, const vector<long long>& poly_sum) :data(&d), left(l), right(r) {
+            hash_val = (poly_sum[right + 1] - poly_sum[left] * pow31[right - left + 1] % MOD + MOD) % MOD;
+        }
+        // must be friend, not member?
+        friend bool operator==(const LongestDupSubStr::string_window_view& lhs, const LongestDupSubStr::string_window_view& rhs) noexcept{
+            if (lhs.hash() !=rhs.hash())
+                return false;
+            return lhs.data_equal(rhs);
+        }
+        long hash() const {
+            return hash_val;
+        }
+    };
+
+    // finding dup string of length L, using hash
+    int longestDupSubstring_helper(const string& s, int L);
 public:
     LongestDupSubStr() {
         const int max_size = 100001;
@@ -43,9 +56,10 @@ public:
         }
         int low = 0, hi = S.size() - 1;
         int dup_pos = -1;
-        int count = 0;
         while (low < hi) {
             int mid = (low + hi) / 2;
+            if (mid == low)  // don't want to be stuck on low!!
+                mid = low+1;
             int dup = longestDupSubstring_helper(S, mid);
             if (dup < 0)  // no dup string at length mid
                 hi = mid - 1;
@@ -53,18 +67,44 @@ public:
                 low = mid;
                 dup_pos = dup;
             }
-            if (++count == 100) {
-                cout << "lo=" << low << " hi " << hi << endl;
-            }
         }
         return dup_pos < 0 ? "" : S.substr(dup_pos-low+1, low);
     }
 };
+namespace std {
+    template<> struct hash<LongestDupSubStr::string_window_view>
+    {
+        typedef LongestDupSubStr::string_window_view argument_type;
+        typedef std::size_t result_type;
+        result_type operator()(argument_type const& s) const noexcept
+        {
+            return s.hash();
+        }
+    };
+}
+
+int LongestDupSubStr::longestDupSubstring_helper(const string& s, int L)
+{
+    if (L == 0)
+        return -1;
+    unordered_set<LongestDupSubStr::string_window_view> substr_hash;
+    substr_hash.reserve((s.size() - L) * 2);
+    for (int right = L - 1; right < s.size(); right++) {  // string s window [left, right] 
+        int left_char = right - L + 1;  // correspond to poly_sum window [left+1, right+1]
+        LongestDupSubStr::string_window_view wv(s, left_char, right, pow31, poly_sum);
+        //long long left_sum = poly_sum[left_char] * pow31[L] % MOD;
+        //long long window_hash = (poly_sum[right + 1] - left_sum + MOD) % MOD;
+        if (substr_hash.count(wv))
+            return right;
+        substr_hash.insert(wv);
+    }
+    return -1;
+}
 
 #include <catch.hpp>
 TEST_CASE("1044. Longest Duplicate Substring", "[BS]")
 {
     CHECK(LongestDupSubStr().longestDupSubstring("banana") == "ana");
     string long_str = "moplvidmaagmsiyyrkchbyhivlqwqsjcgtumqscmxrxrvwsnjjvygrelcbjgbpounhuyealllginkitfaiviraqcycjmskrozcdqylbuejrgfnquercvghppljmojfvylcxakyjxnampmakyjbqgwbyokaybcuklkaqzawageypfqhhasetugatdaxpvtevrigynxbqodiyioapgxqkndujeranxgebnpgsukybyowbxhgpkwjfdywfkpufcxzzqiuglkakibbkobonunnzwbjktykebfcbobxdflnyzngheatpcvnhdwkkhnlwnjdnrmjaevqopvinnzgacjkbhvsdsvuuwwhwesgtdzuctshytyfugdqswvxisyxcxoihfgzxnidnfadphwumtgdfmhjkaryjxvfquucltmuoosamjwqqzeleaiplwcbbxjxxvgsnonoivbnmiwbnijkzgoenohqncjqnckxbhpvreasdyvffrolobxzrmrbvwkpdbfvbwwyibydhndmpvqyfmqjwosclwxhgxmwjiksjvsnwupraojuatksjfqkvvfroqxsraskbdbgtppjrnzpfzabmcczlwynwomebvrihxugvjmtrkzdwuafozjcfqacenabmmxzcueyqwvbtslhjeiopgbrbvfbnpmvlnyexopoahgmwplwxnxqzhucdieyvbgtkfmdeocamzenecqlbhqmdfrvpsqyxvkkyfrbyolzvcpcbkdprttijkzcrgciidavsmrczbollxbkytqjwbiupvsorvkorfriajdtsowenhpmdtvamkoqacwwlkqfdzorjtepwlemunyrghwlvjgaxbzawmikfhtaniwviqiaeinbsqidetfsdbgsydkxgwoqyztaqmyeefaihmgrbxzyheoegawthcsyyrpyvnhysynoaikwtvmwathsomddhltxpeuxettpbeftmmyrqclnzwljlpxazrzzdosemwmthcvgwtxtinffopqxbufjwsvhqamxpydcnpekqhsovvqugqhbgweaiheeicmkdtxltkalexbeftuxvwnxmqqjeyourvbdfikqnzdipmmmiltjapovlhkpunxljeutwhenrxyfeufmzipqvergdkwptkilwzdxlydxbjoxjzxwcfmznfqgoaemrrxuwpfkftwejubxkgjlizljoynvidqwxnvhngqakmmehtvykbjwrrrjvwnrteeoxmtygiiygynedvfzwkvmffghuduspyyrnftyvsvjstfohwwyxhmlfmwguxxzgwdzwlnnltpjvnzswhmbzgdwzhvbgkiddhirgljbflgvyksxgnsvztcywpvutqryzdeerlildbzmtsgnebvsjetdnfgikrbsktbrdamfccvcptfaaklmcaqmglneebpdxkvcwwpndrjqnpqgbgihsfeotgggkdbvcdwfjanvafvxsvvhzyncwlmqqsmledzfnxxfyvcmhtjreykqlrfiqlsqzraqgtmocijejneeezqxbtomkwugapwesrinfiaxwxradnuvbyssqkznwwpsbgatlsxfhpcidfgzrc";
-    CHECK(LongestDupSubStr().longestDupSubstring(long_str) == "ana");
+    CHECK(LongestDupSubStr().longestDupSubstring(long_str) == "akyj");
 }
