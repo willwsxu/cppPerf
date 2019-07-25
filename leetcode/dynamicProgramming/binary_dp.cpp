d71c8ff890ae54962ab6d990da9dc5eb040fd22a
Will XU <willwsxu@gmail.com>
2019-07-24 22:25:35 -0500

    O(N^3) TLE

--- leetcode/dynamicProgramming/binary_dp.cpp
+++ leetcode/dynamicProgramming/binary_dp.cpp
@@ -3,21 +3,25 @@
 
 using namespace std;
 
-int mctFromLeafValues(vector<int>& arr, int i, int j) 
+int mctFromLeafValues(vector<int>& arr, int i, int j, vector<vector<int>> memo)   // O(N^3)
 {
     if (i == j)
         return 0;
-    int result=INT32_MAX;
-	// try all subtree [i, k], [K+1, j]
-    for (int k = i; k < j; k++) {
-        int left_max = *max_element(begin(arr)+i, begin(arr)+k+1);
-        int right_max = *max_element(begin(arr) + k+1, begin(arr) + j + 1);
-        result = min(result, left_max * right_max + mctFromLeafValues(arr, i, k) + mctFromLeafValues(arr, k + 1, j));
+    if (memo[i][j] == 0) {
+        int result = INT32_MAX;
+        // try all subtree [i, k], [K+1, j]
+        for (int k = i; k < j; k++) {
+            int left_max = *max_element(begin(arr) + i, begin(arr) + k + 1);
+            int right_max = *max_element(begin(arr) + k + 1, begin(arr) + j + 1);
+            result = min(result, left_max * right_max + mctFromLeafValues(arr, i, k, memo) + mctFromLeafValues(arr, k + 1, j, memo));
+        }
+        memo[i][j] = result;
     }
-    return result;
+    return memo[i][j];
 }
 int mctFromLeafValues(vector<int>& arr) {
-    return mctFromLeafValues(arr, 0, (int)arr.size()-1);
+    vector<vector<int>> memo(arr.size(), vector<int>(arr.size(), 0));
+    return mctFromLeafValues(arr, 0, (int)arr.size()-1, memo);
 }
 
 #include <catch.hpp>
