2d6b925557950387f2574519cd78bbc1f808d80d
willxu-tt <weisong.xu@tradingtechnologies.com>
2020-02-01 22:52:44 -0600

    update test variant to return int

--- cpp11/test/test_variant.cpp
+++ cpp11/test/test_variant.cpp
@@ -1,43 +1,44 @@
 #include <iostream>
 
 #include <boost/variant.hpp>
-void test_variant()
+int test_variant(bool toggle)
 {
     struct T1 {
-        void fire_test() const {
+        int fire_test() const {
             std::cout << " T1\n";
+            return 1;
         }
     };
     struct T2 {
-        void fire_test() const {
+        int fire_test() const {
             std::cout << " T2\n";
+            return 2;
         }
     };
-    class my_visitor : public boost::static_visitor<void>
+    class my_visitor : public boost::static_visitor<int>
     {
     public:
-        void operator()(const T1& t) const
+        int operator()(const T1& t) const
         {
-            t.fire_test();
+            return t.fire_test();
         }
 
-        void operator()(const T2& t) const
+        int operator()(const T2& t) const
         {
-            t.fire_test();
+            return t.fire_test();
         }
     };
     boost::variant<T1, T2> test;
-    int toggle = 0;
     if (toggle)
         test = T1();
     else
         test = T2();
-    boost::apply_visitor(my_visitor(), test);
+    return boost::apply_visitor(my_visitor(), test);
 }
 
 #include "..\catch.hpp"
 TEST_CASE("variant tests", "[ALLOC]") {
     SECTION("variant test") {
-        test_variant();
+        REQUIRE (test_variant(false)==2);
     }
 }
